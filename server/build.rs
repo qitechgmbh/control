3802a41533fe68ec33582014e5e4bcfd0549077b
Medformatik <dev@fwcr.de>
2025-03-16 17:02:08 +0100

    Delete build.rs

--- server/build.rs
+++
@@ -1,9 +0,0 @@
-use std::process::Command;
-
-fn main() {
-    let command = Command::new("pnpm")
-        .args(&["run", "build"])
-        .current_dir("../../frontend");
-    // .output()
-    // .expect("Failed to build frontend");
-}
