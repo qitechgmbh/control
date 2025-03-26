3162e705d64ff5efec5585c44b7caefc27990f64
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2025-03-26 18:42:57 +0100

    added a simple script to run server with raw sock Capability

---
+++ cargo_run_linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $1 == "release" ]; then
+    echo "building Release Code"
+    cargo build --release
+else
+    echo "building Debug Code"
+    cargo +beta build
+fi
+# set cap for server executable
+sudo setcap cap_net_raw=eip ./target/debug/server
+# run
+./target/debug/server
