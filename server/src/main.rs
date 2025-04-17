91f47d80f05ee9b3baf2d16c411fdb55a6ecb29f
Icedsoda <29167146+1cedsoda@users.noreply.github.com>
2025-04-17 12:05:47 +0200

    use tokio for init_api

--- server/src/main.rs
+++ server/src/main.rs
@@ -13,11 +13,14 @@ pub mod socketio;
 fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
-    let socketio_layer = smol::block_on(init_socketio());
-    let rt = smol::Executor::new();
-    let _ = rt.spawn(async {
-        init_api(APP_STATE.clone(), socketio_layer).await.unwrap();
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+    let socketio_layer = runtime.block_on(init_socketio());
+    runtime.spawn(async {
+        init_api(APP_STATE.clone(), socketio_layer)
+            .await
+            .expect("Failed to run init_api()");
     });
 
-    init_ethercat(APP_STATE.clone());
+    let init_ethercat_result = init_ethercat(APP_STATE.clone());
+    init_ethercat_result.expect("Failed to run init_ethercat()");
 }
