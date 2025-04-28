b217eeed06c86cc36bbc88676e1c17b6a3986e35
Icedsoda <29167146+1cedsoda@users.noreply.github.com>
2025-04-28 17:26:19 +0200

    lsd

--- nixos-install.sh
+++ nixos-install.sh
@@ -2,4 +2,4 @@
 # -f flag forces overwriting if file already exists
 cp -f /etc/nixos/hardware-configuration.nix ./nixos/os/hardware-configuration.nix 
 
-sudo nixos-rebuild switch --flake .#nixos
+sudo nixos-rebuild switch --flake .#nixos --show-trace
