0f0fa335e8fb39caa1f9b8d516235d2716cc21be
Icedsoda <29167146+1cedsoda@users.noreply.github.com>
2025-04-28 17:06:58 +0200

    copy hardware configuration

--- nixos-install.sh
+++ nixos-install.sh
@@ -1,1 +1,5 @@
+# copy /etc/nixos/hardware-configuration.nix to ./nixos/os/hardware-configuration.nix
+# -f flag forces overwriting if file already exists
+cp -f /etc/nixos/hardware-configuration.nix ./nixos/os/hardware-configuration.nix 
+
 sudo nixos-rebuild switch --flake .#nixos
