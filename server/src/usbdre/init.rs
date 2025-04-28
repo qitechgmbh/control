5dc93ef653aa61eaee3b3e6629c3957de45857a6
Anatomik5 <darmenovalisher@gmail.com>
2025-04-28 16:50:37 +0200

    abstract begin

---
+++ server/src/usbdre/init.rs
@@ -0,0 +1,64 @@
+use crate::{
+    app_state::{AppState, APP_STATE},
+    panic::PanicDetails,
+    socketio::main_namespace::{
+        ethercat_interface_discovery_event::EthercatInterfaceDiscoveryEvent, MainNamespaceEvents,
+    },
+};
+use control_core::{
+    ethercat::interface_discovery::discover_ethercat_interface,
+    socketio::namespace::NamespaceCacheingLogic,
+};
+use smol::channel::Sender;
+use std::sync::Arc;
+
+pub fn init_serial(
+    thread_panic_tx: Sender<PanicDetails>,
+    app_state: Arc<AppState>,
+) -> Result<(), anyhow::Error> {
+    // Notify client via socketio
+    let _ = smol::block_on(async move {
+        let main_namespace = &mut APP_STATE
+            .socketio_setup
+            .namespaces
+            .write()
+            .await
+            .main_namespace;
+        let event = EthercatInterfaceDiscoveryEvent::Discovering(true).build();
+        main_namespace.emit_cached(MainNamespaceEvents::EthercatInterfaceDiscoveryEvent(event));
+    });
+
+    // tries to find a suitable interface in a loop
+    let interface = smol::block_on(async {
+        loop {
+            match discover_ethercat_interface().await {
+                Ok(interface) => {
+                    log::info!("Found working interface: {}", interface);
+                    break interface;
+                }
+                Err(_) => {
+                    log::warn!("No working interface found, retrying...");
+                    // wait 1 seconds before retrying
+                    smol::Timer::after(std::time::Duration::from_secs(1)).await;
+                }
+            }
+        }
+    });
+
+    // Notify client via socketio
+    let interface_clone = interface.clone();
+    let _ = smol::block_on(async move {
+        let main_namespace = &mut APP_STATE
+            .socketio_setup
+            .namespaces
+            .write()
+            .await
+            .main_namespace;
+        let event = EthercatInterfaceDiscoveryEvent::Done(interface_clone).build();
+        main_namespace.emit_cached(MainNamespaceEvents::EthercatInterfaceDiscoveryEvent(event));
+    });
+
+    smol::block_on(setup_loop(thread_panic_tx, &interface, app_state.clone()))?;
+
+    Ok(())
+}
--- server/src/usbdre/mod.rs
+++ server/src/usbdre/mod.rs
@@ -1,3 +1,5 @@
 //pub mod dre;
 pub mod config;
 pub mod usb_detection;
+pub mod modbus;
+pub mod init;
