bcaca0f6dd60596c9cc55587ce98996e914b5009
Icedsoda <29167146+1cedsoda@users.noreply.github.com>
2025-04-29 18:09:31 +0200

    fix env vars in electron

--- electron/src/helpers/ipc/environment/environment-listeners.ts
+++ electron/src/helpers/ipc/environment/environment-listeners.ts
@@ -4,14 +4,14 @@ import { ENVIRONMENT_INFO } from "./environment-channels";
 // Environment information
 const environmentInfo = {
   qitechOs: process.env.QITECH_OS === "true",
-  qitechOsGitTimestamp: process.env.QITECH_OS_TIMESTAMP
-    ? isNaN(Date.parse(process.env.QITECH_OS_TIMESTAMP))
+  qitechOsGitTimestamp: process.env.QITECH_OS_GIT_TIMESTAMP
+    ? isNaN(Date.parse(process.env.QITECH_OS_GIT_TIMESTAMP))
       ? undefined
-      : new Date(process.env.QITECH_OS_TIMESTAMP)
+      : new Date(process.env.QITECH_OS_GIT_TIMESTAMP)
     : undefined,
-  qitechOsGitCommit: process.env.QITECH_OS_COMMIT,
-  qitechOsGitAbbrevation: process.env.QITECH_OS_ABBREVATION,
-  qitechOsGitUrl: process.env.QITECH_OS_URL,
+  qitechOsGitCommit: process.env.QITECH_OS_GIT_COMMIT,
+  qitechOsGitAbbrevation: process.env.QITECH_OS_GIT_ABBREVATION,
+  qitechOsGitUrl: process.env.QITECH_OS_GIT_URL,
 };
 
 export function addEnvironmentEventListeners() {
