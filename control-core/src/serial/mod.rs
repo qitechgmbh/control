a138a1b24feab56ce149930a0c3c2bdfbc63f176
Anatomik5 <darmenovalisher@gmail.com>
2025-05-01 17:35:32 +0200

    control-core - serial registry update

--- control-core/src/serial/mod.rs
+++ control-core/src/serial/mod.rs
@@ -1,1 +1,17 @@
 pub mod registry;
+pub mod serial_identification;
+
+
+pub trait Serial: SerialNew {}
+
+pub trait SerialNew {
+    fn new(path: &str) -> Result<Self, anyhow::Error>
+    where
+        Self: Sized;
+}
+
+#[derive(PartialEq)]
+pub struct ProductConfig{
+    pub vendor_id: u16,
+    pub product_id: u16,
+}
--- control-core/src/serial/registry.rs
+++ control-core/src/serial/registry.rs
@@ -1,116 +1,87 @@
-/*
-*@author: Alisher Darmenov
-*@company: QiTech
-*@created: 20.04.2025
-*@last_update: 28.04.2025
-*
-*@description: This module is responsible for serial devices detection and validation, specially made with serialport to avoid complexity and size of tokio_serial
-*/
-
-use serialport::{SerialPortInfo,SerialPortType};
 use std::{
+    any::{Any, TypeId},
     collections::HashMap,
-    sync::{Arc, Mutex},
+    sync::{Arc, RwLock},
 };
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
 
+use anyhow::{Error, Result};
 
+use crate::serial::{
+    ProductConfig,
+    Serial,
+};
 
-pub struct SerialDeviceRegistry{
-    serial_map: Arc<Mutex<HashMap<u64, SerialPortInfo>>>,
+pub type SerialNewFn = Box<
+    dyn Fn(
+            &str
+        ) -> Result<Arc<RwLock<dyn Serial>>, Error>
+        + Send
+        + Sync,
+>;
+
+pub struct SerialRegistry {
+    type_map: HashMap<TypeId, (ProductConfig, SerialNewFn)>,
 }
 
-impl SerialDeviceRegistry {
+impl SerialRegistry {
     pub fn new() -> Self {
         Self {
-            serial_map: Arc::new(Mutex::new(HashMap::new())),
+            type_map: HashMap::new(),
         }
     }
 
-    /*
-    *@return: Option::None if there are no available ports on device
-    *@return: Returns an empty list if no ports found or error occurred and Vec of SerialPortInfo if ports found
-    *
-    *@description: detects all available ports and returns them as list
-    */
-    pub fn update() -> Vec<SerialPortInfo> {
-        serialport::available_ports().unwrap_or_else(|_| Vec::new())
+    pub fn register<T: Serial + 'static>(
+        &mut self,
+        machine_identficiation: ProductConfig,
+    ) {
+        self.type_map.insert(
+            TypeId::of::<T>(),
+            (
+                machine_identficiation,
+                Box::new(|path| {
+                    Ok(Arc::new(RwLock::new(T::new(
+                        path
+                    )?)))
+                }),
+            ),
+        );
     }
 
-    
-    /*
-    *@param: `ports` - List of available ports.
-    *@param: `validator` - Closure that defines whether a port should be selected.
-    *
-    *@return: Filtered list of ports satisfying the validator.
-    *
-    *@description: validate list of given ports and returns list of ports that satisfies given parameters
-    */
-    pub fn validate<F>(ports: Vec<SerialPortInfo>, validator: F) -> Vec<SerialPortInfo>
-    where
-        F: Fn(&SerialPortInfo) -> bool,
-    {
-        ports.into_iter()
-            .filter(|port| validator(port))
-            .collect()
+    pub fn new_machine(
+        &self,
+        path: &str,
+        profuct_config: &ProductConfig,
+    ) -> Result<Arc<RwLock<dyn Serial>>, anyhow::Error> {
+
+        // find serial new function by comparing ProdutConfig
+        let (_, serial_new_fn) = self
+            .type_map
+            .values()
+            .find(|(pc, _)| pc == profuct_config)
+            .ok_or(anyhow::anyhow!(
+                "[{}::MachineConstructor::new_machine] Machine not found",
+                module_path!()
+            ))?;
+
+        // call machine new function by reference
+        (serial_new_fn)(path)
     }
 
-
-    /*  
-    *@param: `validator` - Closure that defines whether a port should be selected.
-    *
-    *@description: This function updates the list of serial ports in the registry.
-     */
-    pub fn update_ports<F>(&self, validator: F)
-    where
-        F: Fn(&SerialPortInfo) -> bool,
-    {
-        let new_ports = Self::update();
-        let valid_ports = Self::validate(new_ports, validator);
-
-        let mut map = self.serial_map.lock().unwrap();
-
-        // Build a new set of current device signatures
-        let current_keys: Vec<u64> = valid_ports.iter()
-            .map(|port| Self::calculate_port_hash(port)) 
-            .collect();
-
-        // Remove entries that are not in the current list
-        map.retain(|key, _| current_keys.contains(key));
-
-        // Add new ports if they are not already in the map
-        for port in valid_ports {
-            let key = Self::calculate_port_hash(&port);
-
-            if !map.contains_key(&key) {
-                map.insert(key, port.clone());
-            }
+    pub fn downcast<T: Serial + 'static>(
+        &self,
+        machine: Arc<RwLock<dyn Serial>>,
+    ) -> Result<Arc<RwLock<T>>, Error> {
+        if TypeId::of::<T>() == machine.type_id() {
+            // transmute Arc
+            let arc = unsafe { Arc::from_raw(Arc::into_raw(machine) as *const RwLock<T>) };
+            Ok(arc)
+        } else {
+            Err(anyhow::anyhow!(
+                "[{}::MachineConstructor::downcast] Machine is not of type {}",
+                module_path!(),
+                std::any::type_name::<T>()
+            ))
         }
-    }
+    }    
 
-    /*
-    *@param: `port` - SerialPortInfo to calculate TypeId for.
-    *
-    *@return: TypeId for the given SerialPortInfo.
-    *
-    *@description: This function calculates a unique TypeId for the given SerialPortInfo.
-    */
-
-    fn calculate_port_hash(port: &SerialPortInfo) -> u64 {
-        let mut hasher = DefaultHasher::new();
-        port.port_name.hash(&mut hasher);
-        if let SerialPortType::UsbPort(ref usb_info) = port.port_type {
-            if let Some(serial) = &usb_info.serial_number {
-                serial.hash(&mut hasher);
-            }
-            usb_info.vid.hash(&mut hasher);
-            usb_info.pid.hash(&mut hasher);
-        }
-        hasher.finish()
-    }
 }
-
-
-
-
