a6371b39a0b22b324c65a61eca3e645814f82c77
Icedsoda <29167146+1cedsoda@users.noreply.github.com>
2025-05-05 14:25:18 +0200

    add memory locking and realtime priority

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,7 @@
 use std::{panic::catch_unwind, process::exit, sync::Arc};
 
 use app_state::AppState;
+use control_core::realtime::lock_memory;
 use env_logger::Env;
 use ethercat::init::init_ethercat;
 use r#loop::init_loop;
@@ -17,6 +18,15 @@ pub mod rest;
 pub mod socketio;
 
 fn main() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    // lock memory
+    if let Err(e) = lock_memory() {
+        log::error!("[{}::main] Failed to lock memory: {:?}", module_path!(), e);
+    } else {
+        log::info!("[{}::main] Memory locked successfully", module_path!());
+    }
+
     // if the program panics we restart all of it
     match catch_unwind(|| main2()) {
         Ok(_) => {
@@ -31,15 +41,13 @@ fn main() {
 }
 
 fn main2() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-
     let app_state = Arc::new(AppState::new());
 
     let (thread_panic_tx, thread_panic_rx) = unbounded::<PanicDetails>();
 
     init_api(thread_panic_tx.clone(), app_state.clone()).expect("Failed to initialize API");
-    init_ethercat(thread_panic_tx.clone(), app_state.clone())
-        .expect("Failed to initialize EtherCAT");
+    // init_ethercat(thread_panic_tx.clone(), app_state.clone())
+    // .expect("Failed to initialize EtherCAT");
     init_loop(thread_panic_tx, app_state).expect("Failed to initialize loop");
 
     smol::block_on(async {
