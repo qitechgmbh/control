50ac55c2c17a736048d036639daf3d204b5879df
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2025-05-26 16:22:20 +0200

    removed rpm.rs

--- control-core/src/uom_extensions/rpm.rs
+++
@@ -1,7 +0,0 @@
-unit! {
-    system: uom::si;
-    quantity: uom::si::rpm;
-
-    @revolution_per_minute_per_second: 0.104_719_755_119_659_77; "rev/min/s", "revolutions per minute per second",
-        "revolutions per minute per second";
-}
