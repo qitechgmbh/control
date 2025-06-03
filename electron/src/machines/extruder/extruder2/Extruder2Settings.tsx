cee244da235f1172eb3e6b67fc4c823834242d29
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2025-06-03 15:30:21 +0200

    frontend is now interactable

--- electron/src/machines/extruder/extruder2/Extruder2Settings.tsx
+++ electron/src/machines/extruder/extruder2/Extruder2Settings.tsx
@@ -4,9 +4,18 @@ import { Label } from "@/control/Label";
 import { SelectionGroupBoolean } from "@/control/SelectionGroup";
 import React from "react";
 import { useExtruder2 } from "./useExtruder";
+import { EditValue } from "@/control/EditValue";
+import { roundToDecimals } from "@/lib/decimal";
 
 export function Extruder2SettingsPage() {
-  const { inverterSetRotation, rotationState } = useExtruder2();
+  const {
+    inverterSetRotation,
+    rotationState,
+    extruderSetPressureLimit,
+    extruderSetPressureLimitIsEnabled,
+    pressureLimitState,
+    pressureLimitEnabledState,
+  } = useExtruder2();
   return (
     <Page>
       <ControlCard className="bg-red" title="Inverter Settings">
@@ -16,6 +25,29 @@ export function Extruder2SettingsPage() {
             optionTrue={{ children: "Forward" }}
             optionFalse={{ children: "Backward" }}
             onChange={inverterSetRotation}
+          />
+        </Label>
+      </ControlCard>
+
+      <ControlCard className="bg-red" title="Extruder Settings">
+        <Label label="Nozzle Pressure Limit">
+          <EditValue
+            value={pressureLimitState}
+            defaultValue={0}
+            unit="rpm"
+            title="Nozzle Pressure Limit"
+            min={0}
+            max={350}
+            renderValue={(value) => roundToDecimals(value, 0)}
+            onChange={extruderSetPressureLimit}
+          />
+        </Label>
+        <Label label="Nozzle Pressure Limit Enabled">
+          <SelectionGroupBoolean
+            value={pressureLimitEnabledState}
+            optionTrue={{ children: "Enabled" }}
+            optionFalse={{ children: "Disabled" }}
+            onChange={extruderSetPressureLimitIsEnabled}
           />
         </Label>
       </ControlCard>
