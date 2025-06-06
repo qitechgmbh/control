7a564123f49ee312b8c8591c87baffe0e38b2475
Icedsoda <29167146+1cedsoda@users.noreply.github.com>
2025-06-06 14:35:42 +0200

    remove handbook & graphs tab

--- electron/src/machines/winder/winder2/Winder2Page.tsx
+++ electron/src/machines/winder/winder2/Winder2Page.tsx
@@ -15,22 +15,10 @@ export function Winder2Page() {
           icon: "lu:CirclePlay",
         },
         {
-          link: "graphs",
-          activeLink: "graphs",
-          title: "Graphs",
-          icon: "lu:ChartSpline",
-        },
-        {
           link: "settings",
           activeLink: "settings",
           title: "Config",
           icon: "lu:Settings",
-        },
-        {
-          link: "manual",
-          activeLink: "manual",
-          title: "Manual",
-          icon: "lu:BookOpen",
         },
       ]}
     />
