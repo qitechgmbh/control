e3dcfaec3ba190d8313f1ffd9bbeab4046733e88
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2025-06-23 18:19:27 +0200

    Revert "Revert "bakcend api""
    This reverts commit 11e965ddeb06dd9bf63d173840f2566f0963c3fe.

--- server/src/machines/extruder1/screw_speed_controller.rs
+++ server/src/machines/extruder1/screw_speed_controller.rs
@@ -24,7 +24,7 @@ const MAX_FREQ: f64 = 60.0;
 
 #[derive(Debug)]
 pub struct ScrewSpeedController {
-    pid: PidController,
+    pub pid: PidController,
     pub target_pressure: Pressure,
     pub target_rpm: AngularVelocity,
     pub inverter: MitsubishiInverterRS485Actor,
@@ -203,6 +203,7 @@ impl ScrewSpeedController {
     pub async fn update(&mut self, now: Instant, is_extruding: bool) {
         self.pressure_sensor.act(now).await;
         self.inverter.act(now).await;
+
         let measured_pressure = self.get_pressure();
 
         if !self.uses_rpm && !is_extruding && self.motor_on {
@@ -237,11 +238,10 @@ impl ScrewSpeedController {
                 .update(error.get::<bar>(), now)
                 .clamp(MIN_FREQ, MAX_FREQ);
 
-            //            println!("error:{} freq:{}", error.get::<bar>(), freq);
-
             let frequency = Frequency::new::<hertz>(freq);
 
             self.last_update = now;
+
             self.inverter.set_frequency_target(frequency);
         }
     }
