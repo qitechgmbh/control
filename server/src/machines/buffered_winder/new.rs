1078e498f805b3c452b66e3d375976a01be7053a
snacj <bbmelvin@gmx.de>
2025-06-30 16:15:54 +0200

    Added folder buffered_winder under server/src/machines/ ; added files act.rs;api.rs;mod.rs;new.rs to buffered_winder and added buffered_winder to mod.rs in server/src/machines

---
+++ server/src/machines/buffered_winder/new.rs
@@ -0,0 +1,34 @@
+use std::time::Instant;
+
+use anyhow::Error;
+use control_core::machines::new::{validate_no_role_dublicates, validate_same_machine_identification_unique, MachineNewParams, MachineNewTrait};
+
+use super::{
+    BufferedWinder, api::BufferedWinderNamespace,
+};
+
+impl MachineNewTrait for BufferedWinder {
+    fn new<'maindevice>(params: &MachineNewParams) -> Result<Self, Error> {
+        // validate general stuff
+        let device_identification = params
+            .device_group
+            .iter()
+            .map(|device_identification| device_identification.clone())
+            .collect::<Vec<_>>();
+        validate_same_machine_identification_unique(&device_identification)?;
+        validate_no_role_dublicates(&device_identification)?;
+
+        //TODO
+
+
+        smol::block_on(async {
+            //TODO
+
+            let buffered_winder: BufferedWinder = Self {
+                    namespace: BufferedWinderNamespace::new(params.socket_queue_tx.clone()),
+                    last_measurement_emit: Instant::now(),
+            };
+            Ok(buffered_winder)
+            })
+    }
+}
