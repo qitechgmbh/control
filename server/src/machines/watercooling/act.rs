980e37ebe05ce1d5f98db8154fc0615e606052ba
Darmenov Alisher <115714693+Anatomik5@users.noreply.github.com>
2025-07-01 18:13:56 +0200

    Refactor WaterCooling: Comment out temperature_controller usage and add tracing logs in init_loop

--- server/src/machines/watercooling/act.rs
+++ server/src/machines/watercooling/act.rs
@@ -8,7 +8,7 @@ impl Actor for WaterCooling {
         now_ts: Instant,
     ) -> std::pin::Pin<Box<dyn std::future::Future<Output = ()> + Send + '_>> {
         Box::pin(async move {
-            self.temperature_controller.update(now_ts).await;
+            //self.temperature_controller.update(now_ts).await;
 
             if self.mode == WaterCoolingMode::Standby {
                 self.turn_cooling_off();
@@ -18,7 +18,7 @@ impl Actor for WaterCooling {
 
             if now.duration_since(self.last_measurement_emit) > Duration::from_secs_f64(1.0 / 60.0)
             {
-                self.emit_cooling(self.temperature_controller.cooling.clone());
+                //self.emit_cooling(self.temperature_controller.cooling.clone());
 
                 self.emit_cooling_element_power();
 
