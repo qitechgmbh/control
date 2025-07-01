dbd2e39442805b803f7fd9bc370f004bcb72428f
Anatomik5 <darmenovalisher@gmail.com>
2025-07-01 15:34:14 +0200

    el40xx

--- ethercat-hal/src/pdo/el40xx.rs
+++ ethercat-hal/src/pdo/el40xx.rs
@@ -7,34 +7,15 @@ use super::RxPdoObject;
 ///
 /// The "Analog Output" holds the output value and status information.
 #[derive(Debug, Clone, Default, PdoObject, PartialEq)]
-#[pdo_object(bits = 32)]
+#[pdo_object(bits = 16)]
 pub struct AnalogOutput {
-    /// Output value (0-32767 typically corresponds to 0-10V or 0-20mA)
+    /// Output value (-32768-32767 typically corresponds to -10 -10V or 0mA-20mA (from 4-20mA for EL402x))
     pub value: i16,
-
-    /// Error flag indicating output issues
-    pub error: bool,
-
-    /// RxPDO state
-    pub rxpdo_state: bool,
-
-    /// If the PDO objects data has changed since the last write
-    pub rxpdo_toggle: bool,
 }
 
 impl RxPdoObject for AnalogOutput {
     fn write(&self, bits: &mut BitSlice<u8, Lsb0>) {
         // Write the output value to bits 0-15
         bits[0..16].store_le(self.value);
-
-        // Write status bits using set() method
-        bits.set(16, self.error);
-        bits.set(17, self.rxpdo_state);
-        bits.set(18, self.rxpdo_toggle);
-
-        // Clear remaining bits
-        for i in 19..32 {
-            bits.set(i, false);
-        }
     }
 }
