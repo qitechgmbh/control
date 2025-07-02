ea42c5b39ced84ab475d401ba389d163064282c7
snacj <bbmelvin@gmx.de>
2025-07-02 13:12:23 +0200

    Updated registry with Buffer1; Renamed BufferedWinder to Buffer1

--- server/src/machines/buffer1/mod.rs
+++ server/src/machines/buffer1/mod.rs
@@ -1,5 +1,5 @@
 use api::{
-    BufferedWinderNamespace, Mode,
+    Buffer1Namespace, Mode,
 };
 use control_core::machines::Machine;
 use std::time::Instant;
@@ -9,20 +9,20 @@ pub mod api;
 pub mod new;
 
 #[derive(Debug)]
-pub struct BufferedWinder {
-    namespace: BufferedWinderNamespace,
+pub struct Buffer1 {
+    namespace: Buffer1Namespace,
     last_measurement_emit: Instant,
 
     mode: Mode,
 }
 
-impl std::fmt::Display for BufferedWinder {
+impl std::fmt::Display for Buffer1 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "BufferedWinder")
     }
 }
-impl Machine for BufferedWinder {}
+impl Machine for Buffer1 {}
 
-impl BufferedWinder {
+impl Buffer1 {
     //TODO
 }
