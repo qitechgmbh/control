92ddcaca5a1332b3ca3edfe275e6e105e0bde494
snacj <bbmelvin@gmx.de>
2025-07-02 16:30:10 +0200

    added button logic to frontend buffer

--- electron/src/machines/buffer/buffer1/Buffer1ControlPage.tsx
+++ electron/src/machines/buffer/buffer1/Buffer1ControlPage.tsx
@@ -4,17 +4,33 @@ import { ControlGrid } from "@/control/ControlGrid";
 import { Label } from "@/control/Label";
 import React from "react";
 
+import { useBuffer1 } from "./useBuffer1";
+
 export function Buffer1ControlPage() {
+    const {
+        bufferGoUp,
+        bufferGoDown
+    } = useBuffer1();
     return(
         <Page>
             <ControlGrid>
-                <Label label="Button Test">
+                <Label label="Buffer go up!">
                     <TouchButton
                         variant="outline"
                         icon="lu:ArrowUpToLine"
+                        onClick={() => bufferGoUp()}
                     >
                     </TouchButton>
-              </Label>
+
+                </Label>
+                <Label label="Buffer go down!">
+                    <TouchButton
+                        variant="outline"
+                        icon="lu:ArrowDownToLine"
+                        onClick={() => bufferGoDown()}
+                    >
+                    </TouchButton>
+                </Label>
 
             </ControlGrid>
         </Page>
