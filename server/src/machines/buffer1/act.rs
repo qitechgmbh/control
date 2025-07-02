9a617669922453e9e1882032a438484ac63187a8
snacj <bbmelvin@gmx.de>
2025-07-02 13:12:23 +0200

    Updated registry with Buffer1; Renamed BufferedWinder to Buffer1

--- server/src/machines/buffer1/act.rs
+++ server/src/machines/buffer1/act.rs
@@ -1,16 +1,17 @@
 use std::time::{Duration, Instant};
 
-use super::BufferedWinder;
+use super::Buffer1;
 use control_core::actors::Actor;
+use tracing::info;
 
-impl Actor for BufferedWinder {
+impl Actor for Buffer1 {
     fn act(
         &mut self,
         now: Instant
     ) -> std::pin::Pin<Box<dyn Future<Output = ()> + Send + '_>> {
         Box::pin(async move {
             // TODO
-
+            info!("asdasd");
             // if last measurement is older than 1 second, emit a new measurement
             if now.duration_since(self.last_measurement_emit) > Duration::from_secs_f64(1.0 / 60.0)
             {
