576e80cc391eae1cc5f68b9584ff45787d95b3ec
snacj <bbmelvin@gmx.de>
2025-07-02 12:35:53 +0200

    Added Buffer1 properties

--- electron/src/machines/properties.ts
+++ electron/src/machines/properties.ts
@@ -216,11 +216,57 @@ export const mock1: MachineProperties = {
   device_roles: [],
 };
 
+export const buffer: MachineProperties = {
+  name: "Buffer",
+  version: "V1",
+  slug: "buffer",
+  icon: "lu:Disc3",
+  machine_identification: {
+    vendor: VENDOR_QITECH,
+    machine: 0x0008,
+  },
+  device_roles: [
+    {
+      role: 0,
+      role_label: "Bus Coupler",
+      allowed_devices: [
+        {
+          vendor_id: 2,
+          product_id: 0x44c2c52,
+          revision: 0x120000,
+        },
+      ],
+    },
+    {
+      role: 1,
+      role_label: "1x Stepper Spool",
+      allowed_devices: [
+        {
+          vendor_id: 2,
+          product_id: 0x1b813052,
+          revision: 0x100034,
+        },
+      ],
+    },
+    {
+      role: 2,
+      role_label: "1x Stepper Puller",
+      allowed_devices: [
+        {
+          vendor_id: 2,
+          product_id: 0x1b773052,
+          revision: 0x10001e,
+        },
+      ],
+    },
+  ],
+};
 export const machineProperties: MachineProperties[] = [
   winder2,
   extruder2,
   laser1,
   mock1,
+  buffer,
 ];
 
 export const getMachineProperties = (
