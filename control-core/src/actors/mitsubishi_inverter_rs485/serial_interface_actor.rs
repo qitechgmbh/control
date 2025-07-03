d7c7fe0bba2ac7f2f57aca6c0abc077f658d989a
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2025-07-03 20:56:10 +0200

    refactored out the act functionanality

--- control-core/src/actors/mitsubishi_inverter_rs485/serial_interface_actor.rs
+++ control-core/src/actors/mitsubishi_inverter_rs485/serial_interface_actor.rs
@@ -1,8 +1,12 @@
 use crate::modbus::{ModbusRequest, ModbusResponse};
 use ethercat_hal::io::serial_interface::{SerialEncoding, SerialInterface};
-use std::{collections::HashMap, time::Duration};
+use std::{
+    collections::HashMap,
+    pin::Pin,
+    time::{Duration, Instant},
+};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct RequestMetaData {
     pub priority: u32,
     pub ignored_times: u32,
@@ -12,22 +16,36 @@ struct RequestMetaData {
 }
 
 #[derive(Debug)]
+pub enum State {
+    /// WaitingForResponse is set after sending a request through the serial_interface
+    WaitingForResponse,
+    /// After Sending a Resuest we need to wait atleast one ethercat cycle
+    /// After one Cycle we check if el6021 status has transmit accepted toggled
+    /// Then we can set state = ReadyToSend
+    WaitingForRequestAccept,
+    /// After Receiving a Response we need to wait atleast one ethercat cycle
+    /// After one Cycle we check if el6021 status has received accepted toggled
+    WaitingForReceiveAccept,
+    /// ReadyToSend is set after receiving the response from the serial_interface
+    ReadyToSend,
+    /// Initial State
+    Uninitialized,
+}
+
+#[derive(Debug)]
 pub struct SerialInterfaceActor {
     pub serial_interface: SerialInterface,
     pub baudrate: Option<u32>,
     pub encoding: Option<SerialEncoding>,
-
     request_map: HashMap<u32, ModbusRequest>,
     // the priority
     request_metadata_map: HashMap<u32, RequestMetaData>,
     pub response: Option<ModbusResponse>,
-}
-
-pub enum SerialConnectionType {
-    Serial,
-    ModbusRTU,
-    ModbusASCII,
-    ModbusTCP,
+    pub state: State,
+    last_message_size: usize,
+    last_message_delay: u32,
+    last_message_id: u32,
+    last_ts: Instant,
 }
 
 impl SerialInterfaceActor {
@@ -39,25 +57,108 @@ impl SerialInterfaceActor {
             request_map: HashMap::new(),
             request_metadata_map: HashMap::new(),
             response: None,
+            last_message_size: 0,
+            last_message_delay: 0,
+            state: State::Uninitialized,
+            last_ts: Instant::now(),
+            last_message_id: 0,
         }
     }
 
     pub fn add_request(
         &mut self,
         request_id: u32,
-        request: ModbusRequest,
         priority: u32,
+        request: ModbusRequest,
         delay: Option<u32>,
     ) {
         self.request_map.insert(request_id, request);
+        if !self.request_metadata_map.contains_key(&request_id) {
+            let meta_data = RequestMetaData {
+                priority,
+                ignored_times: 0,
+                extra_delay: delay,
+            };
+            self.request_metadata_map.insert(request_id, meta_data);
+        }
+    }
 
-        let meta_data = RequestMetaData {
-            priority,
-            ignored_times: 0,
-            extra_delay: delay,
-        };
+    /// This is used internally to read the receive buffer of the el6021
+    fn read_modbus_response(
+        &mut self,
+    ) -> Pin<Box<dyn Future<Output = Result<ModbusResponse, anyhow::Error>> + Send + '_>> {
+        Box::pin(async move {
+            let res: Option<Vec<u8>> = (self.serial_interface.read_message)().await;
+            let raw_response = match res {
+                Some(res) => res,
+                None => {
+                    vec![]
+                }
+            };
 
-        self.request_metadata_map.insert(request_id, meta_data);
+            let response: Result<ModbusResponse, _> =
+                ModbusResponse::try_from(raw_response.clone());
+            match response {
+                Ok(result) => {
+                    self.last_message_size = result.clone().data.len() + 4;
+                    Ok(result)
+                }
+                Err(_) => {
+                    self.last_message_size = 22;
+                    Err(anyhow::anyhow!("error"))
+                }
+            }
+        })
+    }
+
+    /// This is used internally to fill the write buffer of the el6021 with the modbus request
+    /// Decides what requests to send first by finding the one with the highest priority
+    /// For example Highest Priority requests: ResetInverter StopMotor    
+    fn send_modbus_request(&mut self) -> Pin<Box<dyn Future<Output = ()> + Send + '_>> {
+        Box::pin(async move {
+            if self.request_map.len() == 0 {
+                return;
+            };
+
+            let mut highest_prio_request: Option<&ModbusRequest> = None;
+            let mut highest_priority: u32 = 0;
+            let mut highest_id: &u32 = &0;
+            let mut delay: u32 = 0;
+
+            for (key, value) in self.request_metadata_map.iter_mut() {
+                // borrowchecker complaining
+                let priority = value.priority as u32;
+                let ignored_times = value.ignored_times;
+                let effective_priority: u32 = priority as u32 + ignored_times;
+
+                if effective_priority > highest_priority {
+                    highest_prio_request = Some(self.request_map.get(key).unwrap());
+                    highest_priority = effective_priority;
+                    highest_id = key;
+                    delay = match value.extra_delay {
+                        Some(delay) => delay,
+                        None => 0,
+                    }
+                }
+            }
+
+            let request = match highest_prio_request {
+                Some(request) => request,
+                None => return,
+            };
+            let modbus_request: Vec<u8> = request.clone().into();
+
+            let res = (self.serial_interface.write_message)(modbus_request.clone()).await;
+            match res {
+                Ok(_) => {
+                    self.last_message_delay = delay;
+                    self.last_message_id = *highest_id;
+                }
+                Err(_) => tracing::error!("ERROR: serial_interface.write_message has failed"),
+            }
+
+            self.last_message_size = modbus_request.len();
+        })
     }
 
     pub async fn initialize(&self) -> bool {
@@ -73,6 +174,12 @@ impl SerialInterfaceActor {
             Some(_) => (),
         };
         return true;
+    }
+
+    fn set_ignored_times_modbus_requests(&mut self) {
+        for (_, value) in self.request_metadata_map.iter_mut() {
+            value.ignored_times += 1;
+        }
     }
 
     /// Modbus RTU has silent time between frames that needs to be adhered to, if you send before silent_time is over between frames, then there will be lost frames
@@ -108,5 +215,67 @@ impl SerialInterfaceActor {
         full_timeout += silent_time;
 
         return Some(Duration::from_nanos(full_timeout));
+    }
+
+    fn act(&mut self, now_ts: Instant) -> Pin<Box<dyn Future<Output = ()> + Send + '_>> {
+        Box::pin(async move {
+            let elapsed: Duration = now_ts.duration_since(self.last_ts);
+
+            let timeout = self
+                .calculate_modbus_rtu_timeout(
+                    Duration::from_nanos(self.last_message_delay as u64),
+                    self.last_message_size,
+                )
+                .unwrap();
+
+            if elapsed < timeout {
+                return;
+            }
+
+            self.last_ts = now_ts;
+
+            match self.state {
+                State::WaitingForResponse => {
+                    let ret = self.read_modbus_response().await;
+                    match ret {
+                        Ok(ret) => {
+                            self.response = Some(ret);
+                        }
+                        Err(_) => {
+                            self.response = None;
+                            self.state = State::ReadyToSend;
+                        }
+                    }
+                }
+                State::ReadyToSend => {
+                    self.send_modbus_request().await;
+                    self.request_map.remove(&self.last_message_id);
+                    self.request_metadata_map.remove(&self.last_message_id);
+                    self.set_ignored_times_modbus_requests();
+                }
+                State::WaitingForReceiveAccept => {
+                    // Waste atleast one Ethercat Cycle here to ensure that request/response stay in sync
+                    self.state = State::ReadyToSend;
+                }
+                State::WaitingForRequestAccept => {
+                    // An empty vec is used to check if we are finished with writing the message
+                    // This is to keep the Serialinterface more simple
+                    let res = (self.serial_interface.write_message)(vec![]).await;
+                    let finished = match res {
+                        Ok(res) => res,
+                        Err(_) => {
+                            self.state = State::ReadyToSend;
+                            return;
+                        }
+                    };
+
+                    if finished == true {
+                        self.state = State::WaitingForResponse;
+                    }
+                }
+                _ => (),
+            }
+            self.response = None;
+        })
     }
 }
