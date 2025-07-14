dae3def1889d79d3502f538c1ff497aa712be637
snacj <0xsnacj@proton.me>
2025-07-14 17:32:43 +0200

    Added and implemented downcast function

--- control-core/src/machines/mod.rs
+++ control-core/src/machines/mod.rs
@@ -1,8 +1,11 @@
-use crate::machines::new::MachineAct;
+use anyhow::anyhow;
+use crate::actors::Actor;
 use api::MachineApi;
 use new::MachineNewTrait;
+use smol::lock::Mutex;
 use std::any::Any;
 use std::fmt::Debug;
+use std::sync::Arc;
 pub mod api;
 pub mod identification;
 pub mod manager;
@@ -10,4 +13,30 @@ pub mod manager_iter;
 pub mod new;
 pub mod registry;
 
-pub trait Machine: MachineNewTrait + MachineApi + MachineAct + Any + Send + Sync + Debug {}
+pub trait Machine: MachineNewTrait + MachineApi + Actor + Any + Debug {
+    fn as_any(&self) -> &dyn Any;
+}
+/// Casts a `dyn Machine` to a specific machine type
+pub async fn downcast_machine<T: Machine>(
+    machine: Arc<Mutex<dyn Machine>>,
+) -> Result<Arc<Mutex<T>>, anyhow::Error> {
+    // Acquire a read lock on the RwLock
+    let read_lock = machine.lock().await;
+
+    // Check if the inner type can be downcasted to T
+    if read_lock.as_any().is::<T>() {
+        // Clone the Arc and return it as the desired type
+        let cloned_machine = Arc::clone(&machine);
+        // Transmute the Arc to the desired type
+        unsafe {
+            Ok(Arc::from_raw(
+                Arc::into_raw(cloned_machine) as *const Mutex<T>
+            ))
+        }
+    } else {
+        Err(anyhow!(
+            "[{}::downcast_machine] Downcast failed",
+            module_path!()
+        ))
+    }
+}
