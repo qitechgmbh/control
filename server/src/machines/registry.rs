2a6ca54f5350b6c14f4c6f18c54a1f87f067dc44
snacj <0xsnacj@proton.me>
2025-07-14 17:34:20 +0200

    Refactoring

--- server/src/machines/registry.rs
+++ server/src/machines/registry.rs
@@ -1,6 +1,5 @@
 use crate::machines::{
-    MACHINE_EXTRUDER_V1, MACHINE_LASER_V1, MACHINE_MOCK, MACHINE_WINDER_V1, VENDOR_QITECH,
-    extruder1::ExtruderV2, laser::LaserMachine, mock::MockMachine, winder2::Winder2,
+    buffer1::BufferV1, extruder1::ExtruderV2, laser::LaserMachine, mock::MockMachine, mock2::Mock2Machine, winder2::Winder2, MACHINE_BUFFER_V1, MACHINE_EXTRUDER_V1, MACHINE_LASER_V1, MACHINE_MOCK, MACHINE_MOCK2, MACHINE_WINDER_V2, VENDOR_QITECH
 };
 use control_core::machines::{identification::MachineIdentification, registry::MachineRegistry};
 use lazy_static::lazy_static;
@@ -8,10 +7,7 @@ use lazy_static::lazy_static;
 lazy_static! {
     pub static ref MACHINE_REGISTRY: MachineRegistry = {
         let mut mc = MachineRegistry::new();
-        mc.register::<Winder2>(MachineIdentification {
-            vendor: VENDOR_QITECH,
-            machine: MACHINE_WINDER_V1,
-        });
+        mc.register::<Winder2>(Winder2::MACHINE_IDENTIFICATION);
         mc.register::<LaserMachine>(MachineIdentification {
             vendor: VENDOR_QITECH,
             machine: MACHINE_LASER_V1,
@@ -20,10 +16,9 @@ lazy_static! {
             vendor: VENDOR_QITECH,
             machine: MACHINE_EXTRUDER_V1,
         });
-        mc.register::<MockMachine>(MachineIdentification {
-            vendor: VENDOR_QITECH,
-            machine: MACHINE_MOCK,
-        });
+        mc.register::<MockMachine>(MockMachine::MACHINE_IDENTIFICATION);
+        mc.register::<BufferV1>(MockMachine::MACHINE_IDENTIFICATION);
+        mc.register::<Mock2Machine>(Mock2Machine::MACHINE_IDENTIFICATION);
         mc
     };
 }
