8b2b1026c648dd7c33694433ff74334da9a66295
Oshgnacknak <osh@oshgnacknak.de>
2025-07-15 22:33:45 +0200

    Parse presets from local storage via zod

--- electron/src/lib/preset/presetStore.ts
+++ electron/src/lib/preset/presetStore.ts
@@ -1,16 +1,36 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import { Preset, PresetData } from "./preset";
+import { Preset, PresetData, presetSchema } from "./preset";
 import { MachineIdentification } from "@/machines/types";
+import { z } from "zod";
+import { toastError } from "@/components/Toast";
 
-export type PresetStore = {
+const persistedPresetSchema = presetSchema(z.any()).extend({
+  lastModified: z.string(),
+});
+
+const persistedStateSchema = z.object({
+  presets: z.array(persistedPresetSchema),
+  latestPresetIds: z.map(z.string(), z.number()),
+});
+
+const localStoreItemSchema = z.object({
+  state: z.object({
+    presets: z.array(z.any()).default([]),
+    latestPresetIds: z.array(z.tuple([z.string(), z.number()])).default([]),
+  }),
+});
+
+type PresetStoreData = {
   presets: Preset<any>[];
-  latestPresetIds: Map<MachineIdentification, number>;
+  latestPresetIds: Map<string, number>;
+};
 
+export type PresetStore = PresetStoreData & {
   insert: <T extends PresetData>(preset: Omit<Preset<T>, "id">) => Preset<T>;
   update: <T extends PresetData>(preset: Preset<T>) => void;
   remove: <T extends PresetData>(preset: Preset<T>) => void;
-  getById: <T extends PresetData>(id: number) => Preset<T> | undefined;
+  getById: (id: number) => Preset<any> | undefined;
 
   setLatestPresetId: (
     machineIdentification: MachineIdentification,
@@ -30,12 +50,14 @@ const storage = {
     }
 
     try {
-      const parsed = JSON.parse(str);
-      const latestPresetIds = new Map(parsed.state.latestPresetIds);
+      const json = JSON.parse(str);
+      const { state } = localStoreItemSchema.parse(json);
+
+      const latestPresetIds = new Map(state.latestPresetIds);
 
       return {
         state: {
-          ...parsed.state,
+          presets: state.presets,
           latestPresetIds,
         },
       };
@@ -72,7 +94,7 @@ export const usePresetStore = create<PresetStore>()(
       insert: <T extends PresetData>(preset: Omit<Preset<T>, "id">) => {
         const state = get();
         const { presets } = state;
-        const ids = presets.map((p) => p.id);
+        const ids = presets.map((p: Preset<any>) => p.id);
 
         const presetWithId = preset as Preset<T>;
         presetWithId.id = Math.max(0, ...ids) + 1;
@@ -85,7 +107,7 @@ export const usePresetStore = create<PresetStore>()(
 
       update: <T extends PresetData>(preset: Preset<T>) => {
         const state = get();
-        const presets = state.presets.map((p: Preset<T>) =>
+        const presets = state.presets.map((p: Preset<any>) =>
           p.id === preset.id ? preset : p,
         );
         set({ ...state, presets });
@@ -93,7 +115,9 @@ export const usePresetStore = create<PresetStore>()(
 
       remove: <T extends PresetData>(preset: Preset<T>) => {
         const state = get();
-        const presets = state.presets.filter((p: Preset<T>) => p.id !== preset.id);
+        const presets = state.presets.filter(
+          (p: Preset<any>) => p.id !== preset.id,
+        );
         set({ ...state, presets });
       },
 
@@ -122,26 +146,31 @@ export const usePresetStore = create<PresetStore>()(
     {
       name: "preset-storage",
 
-      merge: (persistedState: any, store: PresetStore) => {
-        // TODO: Using zod vaidation here is be the correct way...
+      merge: (persisted: any, store: PresetStore): PresetStore => {
+        try {
+          const persistedState = persistedStateSchema.parse(persisted);
 
-        const { presets: presistedPresets, latestPresetIds } =
-          persistedState as
-            | {
-                presets: Preset<any>[];
-              }
-            | any;
+          const presets = persistedState.presets.map(
+            (preset): Preset<any> => ({
+              ...preset,
+              lastModified: new Date(preset.lastModified),
+            }),
+          );
 
-        const presets = presistedPresets.map((preset: Preset<any>) => ({
-          ...preset,
-          lastModified: new Date(preset.lastModified),
-        }));
+          return {
+            ...store,
+            latestPresetIds: persistedState.latestPresetIds,
+            presets,
+          };
+        } catch (e) {
+          console.error(e);
+          toastError(
+            `Loading presets failed`,
+            `Could not load presets from storage.`,
+          );
+        }
 
-        return {
-          ...store,
-          latestPresetIds,
-          presets,
-        };
+        return store;
       },
 
       storage,
