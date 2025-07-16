03509cf8dda16169826a29f0a555fa4041832931
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2025-07-16 16:45:24 +0200

    increased MAX_FRAMES

--- server/src/ethercat/config.rs
+++ server/src/ethercat/config.rs
@@ -5,6 +5,6 @@ pub const MAX_SUBDEVICES: usize = 16;
 /// Maximum PDU data payload size - set this to the max PDI size or higher.
 pub const MAX_PDU_DATA: usize = PduStorage::element_size(256);
 /// Maximum number of EtherCAT frames that can be in flight at any one time.
-pub const MAX_FRAMES: usize = 16;
+pub const MAX_FRAMES: usize = 32;
 /// Maximum total PDI length.
 pub const PDI_LEN: usize = 256;
