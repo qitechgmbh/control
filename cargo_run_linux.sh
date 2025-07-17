8abd6558d1570b179f94c463ddf05845004fef21
Oshgnacknak <osh@oshgnacknak.de>
2025-07-17 15:12:32 +0200

    Set caps also for real time clock

--- cargo_run_linux.sh
+++ cargo_run_linux.sh
@@ -3,21 +3,23 @@
 FEATURE=""
 
 # Check if "mock-machine" was passed as an argument
-if [ "$1" == "mock-machine" ]; then
+if [[ "$1" == "mock-machine" ]]; then
     echo "Building Debug Code with mock-machine feature"
     FEATURE="--features mock-machine"
+fi
+if [[ "$1" == "release" ]]; then
+    echo "building Release Code"
+    cargo build --release
 else
     echo "Building Debug Code"
+    cargo build $FEATURE
 fi
-
-# Build the project with or without features
-cargo build $FEATURE
 
 # Only setcap if not building with mock-machine
 if [ "$1" != "mock-machine" ]; then
     echo "Setting capabilities for server executable"
-    sudo setcap cap_net_raw=eip ./target/debug/server
+    sudo setcap 'cap_net_raw,cap_sys_nice=eip' ./target/debug/server
 fi
-
-# Run the server
+# run
 ./target/debug/server
+
