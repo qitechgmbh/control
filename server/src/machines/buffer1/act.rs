b68d18dbfb6792aac43f746af4a63315fe1d1fdd
snacj <0xsnacj@proton.me>
2025-07-25 12:09:50 +0200

    Added start of business logic for buffer lift

--- server/src/machines/buffer1/act.rs
+++ server/src/machines/buffer1/act.rs
@@ -1,9 +1,22 @@
 use std::time::{Duration, Instant};
 
+use crate::machines::buffer1::BufferV1Mode;
+
 use super::BufferV1;
 use control_core::machines::new::MachineAct;
+use tracing::info;
+use uom::si::velocity::millimeter_per_second;
 impl MachineAct for BufferV1 {
     fn act(&mut self, now: Instant) {
+            if self.mode == BufferV1Mode::FillingBuffer {
+                let speed = self.buffer_lift_controller.calculate_buffer_lift_speed();
+                // Debug
+                info!(
+                    "Buffer Lift Speed: {:?}",
+                    self.buffer_lift_controller.get_lift_speed().get::<millimeter_per_second>()
+                );
+                self.buffer_lift_controller.update_speed(speed);
+            }
         // if last measurement is older than 1 second, emit a new measurement
         if now.duration_since(self.last_measurement_emit) > Duration::from_secs_f64(1.0 / 30.0) {
             // Emit live values at 30 FPS
--- server/src/machines/buffer1/buffer_lift_controller.rs
+++ server/src/machines/buffer1/buffer_lift_controller.rs
@@ -1,10 +1,29 @@
+use control_core::{
+    converters::linear_step_converter::LinearStepConverter,
+    uom_extensions::velocity::meter_per_minute,
+};
 use ethercat_hal::io::stepper_velocity_el70x1::StepperVelocityEL70x1;
+use uom::{
+    ConstZero,
+    si::{f64::Velocity, velocity::millimeter_per_second},
+};
 
 #[derive(Debug)]
-pub struct BufferTowerController {
+pub struct BufferLiftController {
+    /// Whether the speed controller is enabled or not
     enabled: bool,
     /// Stepper driver. Controls buffer stepper motor
     pub stepper_driver: StepperVelocityEL70x1,
+    // Step Converter
+    pub converter: LinearStepConverter,
+
+    /// Fixed constants
+    spool_amount: u8,
+
+    /// Variables
+    pub current_input_speed: Velocity,
+    pub target_output_speed: Velocity,
+    lift_speed: Velocity,
 }
 
 impl BufferTowerController {
@@ -12,16 +31,44 @@ impl BufferTowerController {
         Self {
             enabled: false,
             stepper_driver: driver,
+            spool_amount: 13,
+            converter,
+
+            current_input_speed: Velocity::ZERO,
+            target_output_speed: Velocity::ZERO,
+            lift_speed: Velocity::ZERO,
         }
     }
+}
+
+impl BufferLiftController {
+    /// Calculate the speed of the buffer lift from current input speed
+    ///
+    /// Formula: input_speed / ( 2 * spool_amount )
+    pub fn calculate_buffer_lift_speed(&mut self) -> Velocity {
+        self.lift_speed = Velocity::new::<millimeter_per_second>(
+            (self.current_input_speed.get::<millimeter_per_second>() - self.target_output_speed.get::<millimeter_per_second>())
+                / (2.0 * self.spool_amount as f64),
+        );
+        self.lift_speed
+    }
+}
+
+impl BufferLiftController {
     pub fn set_enabled(&mut self, enabled: bool) {
         self.enabled = enabled;
-        if enabled {
-            self.stepper_driver.set_enabled(true);
-            let _ = self.stepper_driver.set_speed(10.0);
-        } else {
-            self.stepper_driver.set_enabled(false);
-            let _ = self.stepper_driver.set_speed(0.0);
-        }
+        self.stepper_driver.set_enabled(true);
+    }
+
+    pub fn update_speed(&mut self, speed: Velocity) {
+        let steps = self.converter.velocity_to_steps(speed);
+        let _ = self.stepper_driver.set_speed(steps);
+    }
+
+    pub fn set_current_input_speed(&mut self, speed: f64) {
+        self.current_input_speed = Velocity::new::<meter_per_minute>(speed);
+    }
+    pub fn get_lift_speed(&self) -> Velocity {
+        self.lift_speed
     }
 }
