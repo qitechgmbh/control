46a794d3231bc10db1d7102c7b20b5e8216c9740
snacj <0xsnacj@proton.me>
2025-07-31 10:20:08 +0200

    Implemented Buffer Business Logic

--- server/src/machines/buffer1/new.rs
+++ server/src/machines/buffer1/new.rs
@@ -24,8 +24,9 @@ use ethercat_hal::{
 use uom::si::f64::Length;
 use uom::si::length::centimeter;
 
-use crate::machines::buffer1::BufferV1Mode;
 use crate::machines::buffer1::buffer_lift_controller::BufferLiftController;
+use crate::machines::buffer1::puller_speed_controller::PullerSpeedController;
+use crate::machines::buffer1::{BufferV1Mode, puller_speed_controller};
 use crate::machines::get_ethercat_device;
 
 use super::{BufferV1, api::Buffer1Namespace};
@@ -126,7 +127,13 @@ impl MachineNewTrait for BufferV1 {
                 StepperVelocityEL70x1::new(el7041.clone(), EL7041_0052Port::STM1),
                 LinearStepConverter::from_diameter(200, Length::new::<centimeter>(8.0)),
             );
+            let puller_speed_controller = PullerSpeedController::new(
+                Velocity::new::<meter_per_minute>(1.0),
+                Length::new::<millimeter>(1.75),
+                LinearStepConverter::from_diameter(200, Length::new::<centimeter>(8.0)),
+            );
 
+            // Get machine identification unique
             let machine_id = params
                 .device_group
                 .first()
@@ -136,11 +143,16 @@ impl MachineNewTrait for BufferV1 {
                 .clone();
 
             // create buffer instance
-            let mut buffer: Self = Self {
+            let mut buffer: BufferV1 = Self {
+                puller: StepperVelocityEL70x1::new(
+                    el7031_0030.clone(),
+                    EL7031_0030StepperPort::STM1,
+                ),
                 namespace: Buffer1Namespace::new(params.socket_queue_tx.clone()),
                 last_measurement_emit: Instant::now(),
                 mode: BufferV1Mode::Standby,
                 buffer_lift_controller: buffer_tower_controller,
+                puller_speed_controller: puller_speed_controller,
                 machine_manager: params.machine_manager.clone(),
                 machine_identification_unique: machine_id,
                 connected_winder: None,
