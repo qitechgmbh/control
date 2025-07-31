1907f284f4beb73e6e9b2dd5eca8425ee7962210
snacj <0xsnacj@proton.me>
2025-07-31 11:32:24 +0200

    Refactored to clean Act function in Winder2

--- server/src/machines/winder2/act.rs
+++ server/src/machines/winder2/act.rs
@@ -21,6 +21,9 @@ impl MachineAct for Winder2 {
             self.emit_state();
         }
 
+        // sync the puller speed to the buffer output speed
+        self.sync_buffer_speed();
+
         // more than 33ms have passed since last emit (30 "fps" target)
         if now.duration_since(self.last_measurement_emit) > Duration::from_secs_f64(1.0 / 30.0) {
             self.emit_live_values();
@@ -36,6 +39,7 @@ impl MachineAct for Winder2 {
                         ));
                 }
             }
+
             // check if traverse state changed
             if self.traverse_controller.did_change_state() {
                 self.emit_state();
