9a8dcfb7dba0ee61625b123ecd970b0fcc853393
snacj <0xsnacj@proton.me>
2025-07-31 10:19:46 +0200

    Updated Act function to implement puller sync and buffer input speed

--- server/src/machines/buffer1/act.rs
+++ server/src/machines/buffer1/act.rs
@@ -9,13 +9,21 @@ use uom::si::velocity::millimeter_per_second;
 impl MachineAct for BufferV1 {
     fn act(&mut self, now: Instant) -> std::pin::Pin<Box<dyn Future<Output = ()> + Send + '_>> {
         Box::pin(async move {
+
+            // sync the puller speed
+            self.sync_puller_speed(now);
+
             if self.mode == BufferV1Mode::FillingBuffer {
+                // get the current input speed of the buffer ( = current output speed of the puller)
+                self.buffer_lift_controller.set_current_input_speed(self.puller_speed_controller.get_target_speed().get::<millimeter_per_second>());
+                // calculate the speed of the buffer lift
                 let speed = self.buffer_lift_controller.calculate_buffer_lift_speed();
                 // Debug
                 info!(
                     "Buffer Lift Speed: {:?}",
                     self.buffer_lift_controller.get_lift_speed().get::<millimeter_per_second>()
                 );
+                // update the speed of the buffer lift
                 self.buffer_lift_controller.update_speed(speed);
             }
             // if last measurement is older than 1 second, emit a new measurement
