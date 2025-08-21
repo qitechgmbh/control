a50d1e62c53e6302ad6e9acfd9af761ca628d67c
snacj <0xsnacj@proton.me>
2025-08-21 16:11:29 +0200

    Fixed Comments

--- server/src/machines/winder2/mod.rs
+++ server/src/machines/winder2/mod.rs
@@ -905,7 +905,7 @@ impl Winder2 {
 
 /// implement laser connection
 impl Winder2 {
-    /// set connected buffer
+    /// set connected laser
     pub fn set_connected_laser(
         &mut self,
         machine_identification_unique: MachineIdentificationUnique,
