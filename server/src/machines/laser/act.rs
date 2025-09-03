3421caa965c792422a8f9aad591e43c5d0685576
snacj <0xsnacj@proton.me>
2025-09-03 11:32:13 +0200

    Implemented setting of measured diameter

--- server/src/machines/laser/act.rs
+++ server/src/machines/laser/act.rs
@@ -18,12 +18,17 @@ use std::time::{Duration, Instant};
 /// The method ensures that the diameter value is updated approximately 60 times per second.
 ///
 impl MachineAct for LaserMachine {
-    fn act(&mut self, now: Instant) {
-        // more than 33ms have passed since last emit (30 "fps" target)
-        if now.duration_since(self.last_measurement_emit) > Duration::from_secs_f64(1.0 / 30.0) {
-            self.maybe_emit_state_event();
-            self.emit_live_values();
-            self.last_measurement_emit = now;
-        }
+    fn act(&mut self, now: Instant) -> Pin<Box<dyn Future<Output = ()> + Send + '_>> {
+        Box::pin(async move {
+            // sync the diameter
+            self.set_measured_diameter();
+            // The live values are updated approximately 60 times per second
+            if now.duration_since(self.last_measurement_emit) > Duration::from_secs_f64(1.0 / 30.0)
+            {
+                self.maybe_emit_state_event();
+                self.emit_live_values();
+                self.last_measurement_emit = now;
+            }
+        })
     }
 }
