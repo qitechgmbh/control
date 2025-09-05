e748517bf43bddc931a9fce1babdcef615dce7fb
snacj <0xsnacj@proton.me>
2025-09-05 15:36:54 +0200

    changed PID controller to P controller

--- server/src/machines/winder2/api.rs
+++ server/src/machines/winder2/api.rs
@@ -101,7 +101,7 @@ enum Mutation {
     SetMode(Mode),
 
     // Pid Configure
-    SetSpeedPidSettings(PidSettings),
+    SetDeadPKp(f64),
 
     // Connected Machine
     SetConnectedMachine(MachineIdentificationUnique),
@@ -150,7 +150,7 @@ pub struct StateEvent {
     /// connected machine state
     pub connected_machine_state: MachineCrossConnectionState,
     /// pid settings
-    pub pid_settings: PidSettingsStates,
+    pub pdead_settings_state: PDeadSettingsStates,
 }
 
 #[derive(Serialize, Debug, Clone)]
@@ -220,14 +220,6 @@ pub struct ModeState {
     pub can_wind: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct PidSettings {
-    pub ki: f64,
-    pub kp: f64,
-    pub kd: f64,
-    pub dead: f64,
-}
-
 #[derive(Serialize, Debug, Clone)]
 pub struct TensionArmState {
     /// is zeroed
@@ -262,8 +254,8 @@ pub struct ConnectedMachineState {
 }
 
 #[derive(Serialize, Debug, Clone)]
-pub struct PidSettingsStates {
-    pub speed: PidSettings,
+pub struct PDeadSettingsStates {
+    pub kp: f64,
 }
 
 pub enum Winder2Events {
@@ -359,14 +351,14 @@ impl MachineApi for Winder2 {
             Mutation::DisconnectLaser(machine_identification_unique) => {
                 self.disconnect_laser(machine_identification_unique)
             }
-            Mutation::SetSpeedPidSettings(settings) => {
-                self.configure_speed_pid(settings);
+            Mutation::SetDeadPKp(kp) => {
+                self.configure_p_dead(kp);
             }
         }
         Ok(())
     }
 
-    fn api_event_namespace(&mut self) -> Arc<Mutex<Namespace>> {
-        self.namespace.namespace.clone()
+    fn api_event_namespace(&mut self) -> &mut Namespace {
+        &mut self.namespace.namespace
     }
 }
