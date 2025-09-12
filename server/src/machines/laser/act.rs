ed2f1dda2f7392dd1f2dfbc60213859ca5a582c7
snacj <0xsnacj@proton.me>
2025-09-12 11:55:29 +0200

    Fixed Merge Issue

--- server/src/machines/laser/act.rs
+++ server/src/machines/laser/act.rs
@@ -1,8 +1,6 @@
 use super::LaserMachine;
 use control_core::machines::new::MachineAct;
-use std::{
-    time::{Duration, Instant},
-};
+use std::time::{Duration, Instant};
 
 /// Implements the `MachineAct` trait for the `LaserMachine`.
 ///
@@ -21,17 +19,14 @@ use std::{
 ///
 impl MachineAct for LaserMachine {
     fn act(&mut self, now: Instant) {
-        Box::pin(async move {
-            // sync the diameter
-            self.set_measured_diameter();
+        // sync the diameter
+        self.set_measured_diameter();
         self.update();
-            // The live values are updated approximately 60 times per second
-            if now.duration_since(self.last_measurement_emit) > Duration::from_secs_f64(1.0 / 30.0)
-            {
-                self.maybe_emit_state_event();
-                self.emit_live_values();
-                self.last_measurement_emit = now;
-            }
-        });
+        // The live values are updated approximately 60 times per second
+        if now.duration_since(self.last_measurement_emit) > Duration::from_secs_f64(1.0 / 30.0) {
+            self.maybe_emit_state_event();
+            self.emit_live_values();
+            self.last_measurement_emit = now;
+        }
     }
 }
