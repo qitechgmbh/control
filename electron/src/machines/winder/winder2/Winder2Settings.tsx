df702f8bb93e5371f20045b17e3ac52796a1b86e
snacj <0xsnacj@proton.me>
2025-09-15 12:33:09 +0200

    Removed unwrap and fixed clippy

--- electron/src/machines/winder/winder2/Winder2Settings.tsx
+++ electron/src/machines/winder/winder2/Winder2Settings.tsx
@@ -278,7 +278,7 @@ export function Winder2SettingPage() {
               step={0.001}
               renderValue={(v) => roundToDecimals(v, 2)}
               onChange={setPDeadKp}
-              title="Speed PID KP"
+              title="Speed P KP"
             />
           </Label>
         </ControlCard>
