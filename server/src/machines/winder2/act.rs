6820206d78ae236016de052c8b155ca45e495649
snacj <0xsnacj@proton.me>
2025-09-23 15:38:47 +0200

    fixed emit state after state change

--- server/src/machines/winder2/act.rs
+++ server/src/machines/winder2/act.rs
@@ -16,6 +16,10 @@ impl MachineAct for Winder2 {
         // automatically stops or pulls after N Meters if enabled
         self.stop_or_pull_spool(now);
 
+        if self.traverse_controller.did_change_state() {
+            self.emit_state();
+        }
+
         // more than 33ms have passed since last emit (30 "fps" target)
         if now.duration_since(self.last_measurement_emit) > Duration::from_secs_f64(1.0 / 30.0) {
             self.emit_live_values();
