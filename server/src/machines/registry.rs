f5934efa6c6f374abf1f621f095003517116f4c8
kraemr <93094049+kraemr@users.noreply.github.com>
2025-09-29 15:39:25 +0200

    restored mock-machine functionality

--- server/src/machines/registry.rs
+++ server/src/machines/registry.rs
@@ -1,6 +1,11 @@
+#[cfg(feature = "mock-machine")]
+use crate::machines::{extruder1::mock::ExtruderV2, mock::MockMachine};
+
+#[cfg(not(feature = "mock-machine"))]
+use crate::machines::extruder1::ExtruderV2;
+
 use crate::machines::{
-    aquapath1::AquaPathV1, buffer1::BufferV1, extruder1::ExtruderV2, laser::LaserMachine,
-    mock::MockMachine, winder2::Winder2,
+    aquapath1::AquaPathV1, buffer1::BufferV1, laser::LaserMachine, winder2::Winder2,
 };
 use control_core::machines::registry::MachineRegistry;
 use lazy_static::lazy_static;
@@ -11,6 +16,7 @@ lazy_static! {
         mc.register::<Winder2>(Winder2::MACHINE_IDENTIFICATION);
         mc.register::<LaserMachine>(LaserMachine::MACHINE_IDENTIFICATION);
         mc.register::<ExtruderV2>(ExtruderV2::MACHINE_IDENTIFICATION);
+        #[cfg(feature = "mock-machine")]
         mc.register::<MockMachine>(MockMachine::MACHINE_IDENTIFICATION);
         mc.register::<BufferV1>(BufferV1::MACHINE_IDENTIFICATION);
         mc.register::<AquaPathV1>(AquaPathV1::MACHINE_IDENTIFICATION);
