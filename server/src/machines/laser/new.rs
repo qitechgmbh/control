531e23aced5767fb2f489c1eaa79ea45dd1486ee
Christian <80114847+TheBest6337@users.noreply.github.com>
2025-10-10 16:15:04 +0200

    Refactor downcast logic for laser device to reduce code duplication

--- server/src/machines/laser/new.rs
+++ server/src/machines/laser/new.rs
@@ -14,6 +14,16 @@ use control_core::machines::new::{MachineNewHardware, MachineNewTrait};
 use uom::ConstZero;
 use uom::si::{f64::Length, length::millimeter};
 
+// Helper macro to reduce duplication in downcast logic
+macro_rules! downcast_laser_device {
+    ($device:expr, $device_type:ty, $error_msg:expr) => {
+        match smol::block_on(SERIAL_DEVICE_REGISTRY.downcast_arc_rwlock::<$device_type>($device)) {
+            Ok(laser) => laser,
+            Err(_) => return Err(Error::msg($error_msg)),
+        }
+    };
+}
+
 impl MachineNewTrait for LaserMachine {
     fn new(
         params: &control_core::machines::new::MachineNewParams<'_, '_, '_, '_, '_, '_, '_>,
@@ -26,23 +36,20 @@ impl MachineNewTrait for LaserMachine {
             _ => return Err(Error::msg("Invalid hardware type for LaserMachine")),
         };
 
-        // downcast the hardware_serial to Arc<RwLock<Laser>> or Arc<RwLock<MockLaserDevice>>
+        // Downcast the hardware_serial to the appropriate device type
         #[cfg(not(feature = "laser-mock"))]
-        let laser = match smol::block_on(
-            SERIAL_DEVICE_REGISTRY.downcast_arc_rwlock::<Laser>(hardware_serial.device.clone()),
-        ) {
-            Ok(laser) => laser,
-            Err(_) => return Err(Error::msg("Failed to downcast to Laser")),
-        };
+        let laser = downcast_laser_device!(
+            hardware_serial.device.clone(),
+            Laser,
+            "Failed to downcast to Laser"
+        );
 
         #[cfg(feature = "laser-mock")]
-        let laser = match smol::block_on(
-            SERIAL_DEVICE_REGISTRY
-                .downcast_arc_rwlock::<MockLaserDevice>(hardware_serial.device.clone()),
-        ) {
-            Ok(laser) => laser,
-            Err(_) => return Err(Error::msg("Failed to downcast to MockLaserDevice")),
-        };
+        let laser = downcast_laser_device!(
+            hardware_serial.device.clone(),
+            MockLaserDevice,
+            "Failed to downcast to MockLaserDevice"
+        );
 
         // set laser target configuration
         let laser_target = LaserTarget {
