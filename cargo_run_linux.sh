530327b1e8c113136bb2cb532e7732bcadb46e01
Oshgnacknak <osh@oshgnacknak.de>
2025-10-13 15:23:29 +0200

    Fix caps in cargo run Linux

--- cargo_run_linux.sh
+++ cargo_run_linux.sh
@@ -21,10 +21,12 @@ fi
 # Only setcap if not building with mock-machine
 if [ "$1" != "mock-machine" ]; then
     echo "Setting capabilities for server executable"
-    sudo setcap 'cap_net_raw,cap_sys_nice=eip' ./target/debug/server
+    sudo setcap 'cap_dac_override,cap_net_raw,cap_sys_nice,cap_ipc_lock=eip' ./target/debug/server
 
-    echo "Setting permissions for serial ports"
-#    sudo chown "root:$(groups | cut -f1 -d' ')" /dev/ttyUSB*
+    if compgen -G "/dev/ttyUSB*" > /dev/null; then
+      echo "Setting permissions for serial ports"
+      sudo chown "root:$(groups | cut -f1 -d' ')" /dev/ttyUSB*
+    fi
 fi
 
 # run
