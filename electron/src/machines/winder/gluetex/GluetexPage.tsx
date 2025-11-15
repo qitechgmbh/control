7cbf8d92631789d1870353bd9fad471d82f74dd4
Christian <80114847+TheBest6337@users.noreply.github.com>
2025-11-15 13:56:32 +0100

    Add default winder to gluetex device with dummy data

---
+++ electron/src/machines/winder/gluetex/GluetexPage.tsx
@@ -0,0 +1,42 @@
+import { Topbar } from "@/components/Topbar";
+import React from "react";
+
+export function GluetexPage() {
+  return (
+    <Topbar
+      pathname={`/_sidebar/machines/gluetex`}
+      items={[
+        {
+          link: "control",
+          activeLink: "control",
+          title: "Control",
+          icon: "lu:CirclePlay",
+        },
+        {
+          link: "graphs",
+          activeLink: "graphs",
+          title: "Graphs",
+          icon: "lu:ChartSpline",
+        },
+        {
+          link: "settings",
+          activeLink: "settings",
+          title: "Config",
+          icon: "lu:Settings",
+        },
+        {
+          link: "manual",
+          activeLink: "manual",
+          title: "Manual",
+          icon: "lu:BookOpen",
+        },
+        {
+          link: "presets",
+          activeLink: "presets",
+          title: "Presets",
+          icon: "lu:Save",
+        },
+      ]}
+    />
+  );
+}
