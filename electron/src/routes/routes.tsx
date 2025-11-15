f5ef37b9dd33e58ef24acc54a49bb3dae1c46b57
Christian <80114847+TheBest6337@users.noreply.github.com>
2025-11-15 16:30:53 +0100

    Add Gluetex Heaters page and integrate simulated temperature and power data

--- electron/src/routes/routes.tsx
+++ electron/src/routes/routes.tsx
@@ -246,6 +246,12 @@ export const gluetexAddonsRoute = createRoute({
   component: () => <GluetexAddonsPage />,
 });
 
+export const gluetexHeatersRoute = createRoute({
+  getParentRoute: () => gluetexRoute,
+  path: "heaters",
+  component: () => <GluetexHeatersPage />,
+});
+
 export const laser1SerialRoute = createRoute({
   getParentRoute: () => machinesRoute,
   path: "laser1/$serial",
@@ -449,6 +455,7 @@ export const rootTree = RootRoute.addChildren([
         gluetexGraphsRoute,
         gluetexPresetsRoute,
         gluetexAddonsRoute,
+        gluetexHeatersRoute,
       ]),
 
       extruder2Route.addChildren([
