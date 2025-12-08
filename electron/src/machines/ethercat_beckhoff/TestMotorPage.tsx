5c68e2c78a1b9d0c8b2d31c269278894928d0580
Hamid-el <abdelhamid.elhouari@outlook.de>
2025-12-08 17:20:36 +0100

    finished stepper motor

---
+++ electron/src/machines/ethercat_beckhoff/TestMotorPage.tsx
@@ -0,0 +1,22 @@
+import { Topbar } from "@/components/Topbar";
+import { testMotorSerialRoute } from "@/routes/routes";
+import React from "react";
+
+export function TestMotorPage() {
+    // Hier ist der Zugriff erlaubt, weil diese Funktion erst später aufgerufen wird
+    const { serial } = testMotorSerialRoute.useParams();
+
+    return (
+        <Topbar
+            pathname={`/_sidebar/machines/testmotor/${serial}`}
+            items={[
+                {
+                    link: "control",
+                    activeLink: "control",
+                    title: "Control",
+                    icon: "lu:CirclePlay",
+                },
+            ]}
+        />
+    );
+}
