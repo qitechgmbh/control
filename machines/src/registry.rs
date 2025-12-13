b69645bb5dcec84ac5a716087bb0be6fdf930ba0
snacj <0xsnacj@proton.me>
2025-12-13 14:58:16 +0100

    Fixed Imports

--- machines/src/registry.rs
+++ machines/src/registry.rs
@@ -14,7 +14,7 @@ use crate::{
 };
 
 use crate::{
-    Machine, MachineNewParams, MachineNewTrait, machine_identification::MachineIdentification,
+    Machine, MachineNewParams, MachineNewTrait, machine_identification::MachineIdentification, test_machine::TestMachine
 };
 
 #[cfg(not(feature = "mock-machine"))]
