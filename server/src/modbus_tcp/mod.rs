05c83fed4414b58e2152c27ebe412e2e4d6b5d89
Oshgnacknak <osh@oshgnacknak.de>
2025-12-14 20:00:53 +0100

    Read live values from wago power supply

--- server/src/modbus_tcp/mod.rs
+++ server/src/modbus_tcp/mod.rs
@@ -2,21 +2,39 @@ use std::sync::Arc;
 use crate::app_state::SharedState;
 use machines::{MACHINE_WAGO_POWER_V1, Machine, MachineChannel, VENDOR_QITECH, machine_identification::{MachineIdentification, MachineIdentificationUnique}, wago_power::WagoPower};
 use smol::future;
+use control_core::ethernet::modbus_tcp_discovery::probe_modbus_tcp;
+use control_core::futures::FutureIteratorExt;
 
-// #[cfg(not(feature = "mock-machine"))]
-// pub async fn start_modbus_tcp_discovery(shared_state: Arc<SharedState>) {
-//     loop {
-//         let addresses = probe_modbus_tcp().await;
+#[cfg(not(feature = "mock-machine"))]
+pub async fn start_modbus_tcp_discovery(shared_state: Arc<SharedState>) {
+    let addresses = probe_modbus_tcp().await;
 
-//         for addr in addresses.into_iter() {
+    let machines: Vec<Box<dyn Machine>> = addresses
+        .into_iter()
+        .map(|addr| smol::spawn(
+                async move {
+                let machine_identification_unique = MachineIdentificationUnique {
+                    machine_identification: MachineIdentification {
+                        vendor: VENDOR_QITECH,
+                        machine: MACHINE_WAGO_POWER_V1,
+                    },
+                    serial: 0xbeef,
+                };
 
-//         }
+                let channel = MachineChannel::new(machine_identification_unique);
+                let power = WagoPower::new(channel, addr)
+                    .await
+                    .expect("Failed to initialize wago power supply");
+                Box::new(power) as Box<dyn Machine>
+            })
+        )
+        .join_all()
+        .await;
 
-//         smol::Timer::after(Duration::from_secs(1)).await;
-//     }
-// }
+    shared_state.add_machines(machines).await;
+}
 
-// #[cfg(feature = "mock-machine")]
+#[cfg(feature = "mock-machine")]
 pub async fn start_modbus_tcp_discovery(shared_state: Arc<SharedState>) {
     let machine_identification_unique = MachineIdentificationUnique {
         machine_identification: MachineIdentification {
@@ -34,8 +52,4 @@ pub async fn start_modbus_tcp_discovery(shared_state: Arc<SharedState>) {
     let machines: Vec<Box<dyn Machine>> = vec![Box::new(power)];
 
     shared_state.add_machines(machines).await;
-
-    loop {
-        future::yield_now().await;
-    }
 }
