2e6c44dad62fa4250788762d896c95fd49e00148
nixos-install.sh <nixos-install.sh@localhost>
2025-12-20 19:59:15 +0100

    Give qitech user serial permissions

--- nixos/os/configuration.nix
+++ nixos/os/configuration.nix
@@ -270,7 +270,7 @@ in
   users.users.qitech = {
     isNormalUser = true;
     description = "QiTech HMI";
-    extraGroups = [ "networkmanager" "wheel" "realtime" "wireshark" ];
+    extraGroups = [ "networkmanager" "wheel" "realtime" "wireshark" "dailout" ];
     packages = with pkgs; [ ];
   };
 
