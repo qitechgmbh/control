8a3f312675eeec0c4a607fd3810aae386269f2fb
kraemr <93094049+kraemr@users.noreply.github.com>
2026-01-07 18:01:14 +0100

    Revert "Merge pull request #943 from qitechgmbh/892-add-datasheets-to-the-machines-manual-tab"
    This reverts commit 40e17ef5f1e0a8b12a94af47da2707a4db1654d4, reversing
    changes made to f65d100cd5ed5612f450daa23b60761532255598.

--- electron/src/machines/extruder/extruder2/Extruder2Manual.tsx
+++ electron/src/machines/extruder/extruder2/Extruder2Manual.tsx
@@ -1,15 +1,12 @@
 import { Page } from "@/components/Page";
 import { MarkdownWithToc } from "@/components/MarkdownWithToc";
 import React from "react";
-import extruderManualContent from "@root/docs/machines/manuals/extruder.md?raw";
+import extruderManualContent from "@/assets/markdown/extruder/manual.md?raw";
 
 export function ExtruderV2ManualPage() {
   return (
     <Page>
-      <MarkdownWithToc
-        markdownContent={extruderManualContent}
-        isManual={true}
-      />
+      <MarkdownWithToc markdownContent={extruderManualContent} />
     </Page>
   );
 }
