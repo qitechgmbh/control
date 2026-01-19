f86d0fb2d8e539a1987538c57eb6be3f94efa377
Oshgnacknak <osh@oshgnacknak.de>
2026-01-19 15:54:52 +0100

    Fix rust enum zod check

--- electron/src/lib/types.ts
+++ electron/src/lib/types.ts
@@ -1,32 +1,17 @@
 import { z } from "zod";
 
-export function rustEnumSchema<T extends Record<string, z.ZodType>>(
-  schemas: T,
-) {
-  const shape: any = {};
+export const rustEnum: z.core.CheckFn<object> = (ctx) => {
+  const definedKeys = Object.keys(ctx.value).filter(
+    (key) => ctx.value[key] !== undefined,
+  );
 
-  for (const [key, schema] of Object.entries(schemas)) {
-    if (!schema) {
-      //cath undefined 'undefined' early
-      throw new Error(
-        `rustEnumSchema: Schema for key "${key}" is undefined. Check for circular imports.`,
-      );
-    }
-    shape[key] = schema.optional();
+  if (definedKeys.length === 1) {
+    return;
   }
 
-  const objectSchema = z.object(shape);
-
-  return objectSchema.refine(
-    (data) => {
-      const definedKeys = Object.keys(data).filter(
-        (key) => data[key] !== undefined,
-      );
-      return definedKeys.length === 1;
-    },
-    {
-      message: "Exactly one property must be defined",
-      path: ["_exclusive"],
-    },
-  );
+  ctx.issues.push({
+    code: "custom",
+    message: "Exactly one property must be defined",
+    input: definedKeys
+  });
 }
