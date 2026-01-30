e7f1aa2c784b5ea39eba28a3f64542a266c94e00
snacj <0xsnacj@proton.me>
2026-01-30 18:32:29 +0100

    Converted 671 Stepper to 672 Stepper

--- ethercat-hal/src/devices/wago_750_354.rs
+++ ethercat-hal/src/devices/wago_750_354.rs
@@ -14,6 +14,7 @@ use crate::{
             wago_750_652::{WAGO_750_652_MODULE_IDENT, WAGO_750_652_PRODUCT_ID},
             wago_750_1506::{WAGO_750_1506_MODULE_IDENT, WAGO_750_1506_PRODUCT_ID},
             wago_750_671::{WAGO_750_671_MODULE_IDENT, WAGO_750_671_PRODUCT_ID},
+            wago_750_672::{WAGO_750_672_MODULE_IDENT, WAGO_750_672_PRODUCT_ID},
         },
     },
     helpers::ethercrab_types::EthercrabSubDevicePreoperational,
@@ -252,7 +253,11 @@ impl Wago750_354 {
                 WAGO_750_671_PRODUCT_ID => {
                     module.has_tx = true;
                     module.has_rx = true;
-            }
+                }
+                WAGO_750_672_PRODUCT_ID => {
+                    module.has_tx = true;
+                    module.has_rx = true;
+                }
                 _ => println!(
                     "Wago-750-354 found Unknown/Unimplemented Module: {}",
                     ident_iom
@@ -306,6 +311,9 @@ impl Wago750_354 {
                             Arc::new(RwLock::new(wago_750_402::Wago750_402::new()))
                         WAGO_750_671_MODULE_IDENT => {
                             Arc::new(RwLock::new(wago_750_671::Wago750_671::new()))
+                        }
+                        WAGO_750_672_MODULE_IDENT => {
+                            Arc::new(RwLock::new(wago_750_672::Wago750_672::new()))
                         }
                         _ => {
                             println!(
