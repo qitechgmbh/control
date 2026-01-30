c375820e003b9230dbdf4a3e7bd23a607b3e4fd7
Saurabh Bagade <saurabhbagade@MacBookPro.fritz.box>
2026-01-30 13:58:41 +0100

    fakehash in electron.nix

--- nixos/packages/electron.nix
+++ nixos/packages/electron.nix
@@ -10,7 +10,7 @@ buildNpmPackage rec {
   ELECTRON_SKIP_BINARY_DOWNLOAD = 1;
 
   makeCacheWritable = true;
-  npmDepsHash = "sha256-wzVjsgcZsViPOyQKlKTb5gJIeY416KyCSiheSXSRZoc=";
+  npmDepsHash = lib.fakeHash;
   npmFlags = [ "--no-audit" "--no-fund" ];
 
   installPhase = ''
