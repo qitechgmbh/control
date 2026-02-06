b919721c3aaf77630eac875f8fa95896184fbc44
Achraf Jalleli <150543929+JalleliAchraf@users.noreply.github.com>
2026-02-06 16:20:26 +0100

    Update npmDepsHash for electron package

--- nixos/packages/electron.nix
+++ nixos/packages/electron.nix
@@ -10,7 +10,7 @@ buildNpmPackage rec {
   ELECTRON_SKIP_BINARY_DOWNLOAD = 1;
 
   makeCacheWritable = true;
-  npmDepsHash = "sha256-/7zfLof91S97OmW8dBeaVajYEv3r2eYOZ8Zb2ahykaQ=";
+  npmDepsHash = "sha256-rnaAsSkoTRoBgLCTF8ZFpi7uUQ+8W+u3qoMxsptX6NU=";
   npmFlags = [ "--no-audit" "--no-fund" ];
 
   installPhase = ''
