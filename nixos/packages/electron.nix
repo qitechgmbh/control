a60a1126b05639d4faedd17e1abd6da3549b924f
Achraf Jalleli <150543929+JalleliAchraf@users.noreply.github.com>
2026-03-06 21:40:46 +0100

    Merge pull request #1212 from qitechgmbh/1199-npm-deps-bumps-resolved
    1199 npm deps bumps resolved

--- nixos/packages/electron.nix
+++ nixos/packages/electron.nix
@@ -10,7 +10,7 @@ buildNpmPackage rec {
   ELECTRON_SKIP_BINARY_DOWNLOAD = 1;
 
   makeCacheWritable = true;
-  npmDepsHash = "sha256-YCTHkPVCbs3N2QAralYMCh7GvCYsB6nN2Ab0NJ+t7JU=";
+  npmDepsHash = "sha256-1QAtF2Sp5jh1eUExWsCf2tsaRcAMhL/5QnkcrD3kva4=";
   npmFlags = [ "--no-audit" "--no-fund" ];
 
   installPhase = ''
