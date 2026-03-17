9bf251ebdf122480940b42be23d9cd12b17a9424
just-some-entity <entity@jsentity.dev>
2026-03-17 15:33:28 +0100

    fix ui stuff

--- electron/src/machines/stahlwerk/ff01/pages/ControlPage.tsx
+++ electron/src/machines/stahlwerk/ff01/pages/ControlPage.tsx
@@ -28,115 +28,35 @@ export function ControlPage() {
     <Page>
       <ControlGrid columns={2}>
         <ControlCard title="Measurements">
-          <Label label={state?.weighted_item.name ?? "Waiting for data..."}>
-            <DisplayValue
-              title="Target Weight"
-              icon="lu:Scale"
-              unit="kg"
-              value={state?.weighted_item.weight}
-              renderValue={(v) => v.toFixed(0)}
-            />
-
+          <Label label="Current Weight">
             <TimeSeriesValueNumeric
               label="Current Weight"
               unit="kg"
               renderValue={(v) => v.toFixed(0)}
-              timeseries={motorScrewRpm}
+              timeseries={weightPrev}
             />
-
           </Label>
-
-          
-
+          <Label label="Peak Weight">
+            <TimeSeriesValueNumeric
+              label="Peak Weight"
+              unit="kg"
+              renderValue={(v) => v.toFixed(0)}
+              timeseries={weightPeak}
+            />
+          </Label>      
         </ControlCard>
 
-
-        <ControlCard title="Current">
-          <TimeSeriesValueNumeric
-            label="Live Weight"
-            unit="kg"
-            timeseries={currentWeight}
-            renderValue={(value) => value.toFixed(1)}
-          />
-          <TimeSeriesValueNumeric
-            label="Total Weight"
-            unit="kg"
-            timeseries={totalWeight}
-            renderValue={(value) => value.toFixed(1)}
-          />
-        </ControlCard>
         <ControlCard title="Plate Counter">
           <DisplayValue
             title="Target Quantity"
             icon="lu:Tally1"
             unit="pcs"
-            value={state?.weighted_item.quantity}
+            value={state?.plates_counted}
             renderValue={(v) => v.toFixed(1)}
           />
-          <TimeSeriesValueNumeric
-            label="Current Quantity"
-            unit="pcs"
-            timeseries={plateCounter}
-            renderValue={(value) => value.toFixed(0)}
-          />
-          <TouchButton
-            variant="outline"
-            icon="lu:RotateCcw"
-            onClick={zeroCounters}
-          >
-            Zero Counters
-          </TouchButton>
         </ControlCard>
-        <ControlCard title="Access & Configuration">
-          <Label label="Password">
-            <EditString
-              title="Enter Password"
-              value={state?.configuration.password ?? ""}
-              onChange={(val) => setPassword(val)}
-            />
-          </Label>
-          <Label label="Configuration String">
-            <EditString
-              title="Configuration String"
-              value={state?.configuration.config_string ?? ""}
-              onChange={(val) => setStringValue(val)}
-            />
-          </Label>
-          <Label label="Set Upper Tolerance">
-            <EditValue
-              title="Set Tolerance"
-              value={state?.xtrem_zebra_state.tolerance}
-              unit="kg"
-              step={0.1}
-              min={0}
-              max={1000}
-              renderValue={(value) => value.toFixed(2)}
-              onChange={(val) => setTolerance(val)}
-              defaultValue={defaultState?.xtrem_zebra_state.tolerance}
-            />
-          </Label>
-          <Label label="Set Lower Tolerance">
-            <EditValue
-              title="Set Tolerance"
-              value={state?.xtrem_zebra_state.tolerance}
-              unit="kg"
-              step={0.1}
-              min={0}
-              max={1000}
-              renderValue={(value) => value.toFixed(2)}
-              onChange={(val) => setTolerance(val)}
-              defaultValue={defaultState?.xtrem_zebra_state.tolerance}
-            />
-          </Label>
-          <Label label="Start Weighted Item">
-            <TouchButton
-              variant="outline"
-              icon="lu:ArrowBigRight"
-              onClick={start}
-            >
-              Start
-            </TouchButton>
-          </Label>
+
+        <ControlCard title="Configuration">
           <Label label="Lights">
             <TouchButton
               variant="outline"
@@ -147,7 +67,7 @@ export function ControlPage() {
             </TouchButton>
           </Label>
           <Label label="Tare">
-            <TouchButton variant="outline" icon="lu:Scale" onClick={setTare}>
+            <TouchButton variant="outline" icon="lu:Scale" onClick={tare}>
               Tare Scales
             </TouchButton>
           </Label>
