902dc39ad62767240b3c15e50142dcefe2698dd3
kraemr <93094049+kraemr@users.noreply.github.com>
2026-03-24 16:00:07 +0100

    updated digital input test

--- machine_implementations/src/minimal_machines/digital_input_test_machine/new.rs
+++ machine_implementations/src/minimal_machines/digital_input_test_machine/new.rs
@@ -2,29 +2,18 @@ use super::DigitalInputTestMachine;
 use qitech_lib::{
     ethercat_hal::{
         EtherCATThreadChannel,
-        devices::{EthercatDevice, wago_modules::wago_750_402::Wago750_402},
+        devices::{EthercatDevice, downcast_subdevice, el1008::{self, EL1008}},
     },
     machines::{MachineIdentification, MachineIdentificationUnique},
 };
-
-pub fn downcast_device<T: EthercatDevice>(
-    dev: T,
-    identity: (u32, u32, u32),
-) -> Result<T, anyhow::Error> {
-    match identity {
-        (0, 0, 0) => Ok(dev),
-        _ => Err(anyhow::anyhow!("error")),
-    }
-}
 
 impl DigitalInputTestMachine {
     pub fn new(
         hw: &Vec<Box<dyn EthercatDevice>>,
         _eth_channel: EtherCATThreadChannel,
     ) -> Result<DigitalInputTestMachine, anyhow::Error> {
-        let dev = hw.get(1).unwrap();
-        let downcasted_ref: &Wago750_402 = dev.as_any().downcast_ref::<Wago750_402>().unwrap();
-        let res = downcast_device::<Wago750_402>(downcasted_ref.clone(), (0u32, 0u32, 0u32))?;
+
+        let el1008 : EL1008 = downcast_subdevice::<EL1008>(*hw.get(1).unwrap())?;
         let my_test = Self {
             machine_identification_unique: MachineIdentificationUnique {
                 machine_ident: MachineIdentification {
