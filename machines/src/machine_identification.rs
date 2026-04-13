e41403211ee1ff6fa0c883d1e5d64032422f435d
kraemr <93094049+kraemr@users.noreply.github.com>
2026-04-13 11:19:38 +0200

    updated slug

--- machines/src/machine_identification.rs
+++ machines/src/machine_identification.rs
@@ -62,6 +62,7 @@ impl MachineIdentification {
             x if x == TEST_MACHINE => "test_machine".to_string(),
             x if x == IP20_TEST_MACHINE => "ip20_test_machine".to_string(),
             x if x == ANALOG_INPUT_TEST_MACHINE => "analog_input_test_machine".to_string(),
+            x if x == TEST_MACHINE_STEPPER => "test_machine_stepper".to_string(),
             x if x == WAGO_AI_TEST_MACHINE => "wago_ai_test_machine".to_string(),
             x if x == WAGO_DO_TEST_MACHINE => "wago_do_test_machine".to_string(),
             x if x == WAGO_750_430_DI_MACHINE => "wago_750_430_di_machine".to_string(),
@@ -189,6 +190,7 @@ use crate::MACHINE_MOCK;
 use crate::MACHINE_WAGO_POWER_V1;
 use crate::MACHINE_WINDER_V1;
 use crate::TEST_MACHINE;
+use crate::TEST_MACHINE_STEPPER;
 use crate::VENDOR_QITECH;
 use crate::WAGO_750_430_DI_MACHINE;
 use crate::WAGO_750_531_MACHINE;
