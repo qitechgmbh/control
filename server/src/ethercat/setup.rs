4991fc889bd8400e0dade42bcd87e00cf24301db
kraemr <93094049+kraemr@users.noreply.github.com>
2026-04-13 12:24:23 +0200

    removed unused import

--- server/src/ethercat/setup.rs
+++ server/src/ethercat/setup.rs
@@ -9,7 +9,7 @@ use crate::{
 use control_core::realtime::set_core_affinity;
 use control_core::socketio::namespace::NamespaceCacheingLogic;
 #[cfg(all(target_os = "linux", not(feature = "development-build")))]
-use control_core::{irq_handling::set_irq_affinity, realtime::set_realtime_priority};
+use control_core::{realtime::set_realtime_priority};
 use ethercat_hal::debugging::diagnosis_history::get_most_recent_diagnosis_message;
 use ethercat_hal::devices::devices_from_subdevices;
 
