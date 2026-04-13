2e8c9a91957c89cf58105f391f59d895ca813f9a
kraemr <93094049+kraemr@users.noreply.github.com>
2026-04-13 11:44:24 +0200

    fixed test_machine_stepper to use both wago terminals

---
+++ machines/src/minimal_machines/test_machine_stepper/mod.rs
@@ -0,0 +1,106 @@
+use crate::machine_identification::{MachineIdentification, MachineIdentificationUnique};
+use crate::minimal_machines::test_machine_stepper::api::{StateEvent, TestMachineStepperEvents};
+use crate::{AsyncThreadMessage, Machine, MachineMessage};
+use control_core::socketio::namespace::NamespaceCacheingLogic;
+use ethercat_hal::io::stepper_velocity_wago_750_671::StepperVelocityWago750671;
+use ethercat_hal::io::stepper_velocity_wago_750_672::StepperVelocityWago750672;
+use smol::channel::{Receiver, Sender};
+use std::time::Instant;
+pub mod act;
+pub mod api;
+pub mod new;
+use crate::minimal_machines::test_machine_stepper::api::TestMachineStepperNamespace;
+use crate::{TEST_MACHINE_STEPPER, VENDOR_QITECH};
+
+
+#[derive(Debug)]
+pub enum Stepper {
+    Wago750_672(StepperVelocityWago750672),
+    Wago750_671(StepperVelocityWago750671),
+}
+
+#[derive(Debug)]
+pub struct TestMachineStepper {
+    pub api_receiver: Receiver<MachineMessage>,
+    pub api_sender: Sender<MachineMessage>,
+    pub machine_identification_unique: MachineIdentificationUnique,
+    pub namespace: TestMachineStepperNamespace,
+    pub last_state_emit: Instant,
+    pub main_sender: Option<Sender<AsyncThreadMessage>>,
+    pub stepper: Stepper,
+}
+
+impl Machine for TestMachineStepper {
+    fn get_machine_identification_unique(&self) -> MachineIdentificationUnique {
+        self.machine_identification_unique.clone()
+    }
+
+    fn get_main_sender(&self) -> Option<Sender<AsyncThreadMessage>> {
+        self.main_sender.clone()
+    }
+}
+impl TestMachineStepper {
+    pub const MACHINE_IDENTIFICATION: MachineIdentification = MachineIdentification {
+        vendor: VENDOR_QITECH,
+        machine: TEST_MACHINE_STEPPER,
+    };
+}
+
+impl TestMachineStepper {
+    pub fn get_state(&self) -> StateEvent {
+        match &self.stepper {
+            Stepper::Wago750_672(stepper_velocity_wago750672) => 
+            StateEvent {
+                target_speed: stepper_velocity_wago750672.target_velocity,
+                enabled: stepper_velocity_wago750672.enabled,
+                freq: stepper_velocity_wago750672.freq_range_sel,
+                acc_freq: stepper_velocity_wago750672.acc_range_sel,
+            },
+            Stepper::Wago750_671(stepper_velocity_wago750671) =>             
+            StateEvent {
+                target_speed: stepper_velocity_wago750671.target_velocity,
+                enabled: stepper_velocity_wago750671.enabled,
+                freq: stepper_velocity_wago750671.freq_range_sel,
+                acc_freq: stepper_velocity_wago750671.acc_range_sel,
+            },
+        }
+    }
+
+    pub fn emit_state(&mut self) {
+        let event = self.get_state().build();
+
+        self.namespace.emit(TestMachineStepperEvents::State(event));
+    }
+
+    pub fn set_target_speed(&mut self, speed: i16) {
+        match &mut self.stepper {
+            Stepper::Wago750_672(stepper_velocity_wago750672) => stepper_velocity_wago750672.set_velocity(speed),
+            Stepper::Wago750_671(stepper_velocity_wago750671) => stepper_velocity_wago750671.set_velocity(speed),
+        };        
+        self.emit_state();
+    }
+
+    pub fn set_enabled(&mut self, enabled: bool) {
+        match &mut self.stepper {
+            Stepper::Wago750_672(stepper_velocity_wago750672) => stepper_velocity_wago750672.set_enabled(enabled),
+            Stepper::Wago750_671(stepper_velocity_wago750671) => stepper_velocity_wago750671.set_enabled(enabled),
+        };        
+        self.emit_state();
+    }
+
+    pub fn set_freq(&mut self, factor: u8) {
+        match &mut self.stepper {
+            Stepper::Wago750_672(stepper_velocity_wago750672) => stepper_velocity_wago750672.set_freq_range_sel(factor),
+            Stepper::Wago750_671(stepper_velocity_wago750671) => stepper_velocity_wago750671.set_freq_range_sel(factor),
+        };        
+        self.emit_state();
+    }
+
+    pub fn set_acc_freq(&mut self, factor: u8) {
+        match &mut self.stepper {
+            Stepper::Wago750_672(stepper_velocity_wago750672) => stepper_velocity_wago750672.set_acc_range_sel(factor),
+            Stepper::Wago750_671(stepper_velocity_wago750671) => stepper_velocity_wago750671.set_acc_range_sel(factor),
+        };   
+        self.emit_state();
+    }
+}
---
+++ machines/src/minimal_machines/test_machine_stepper/new.rs
@@ -0,0 +1,113 @@
+use crate::minimal_machines::test_machine_stepper::{
+    TestMachineStepper, api::TestMachineStepperNamespace,
+};
+use ethercat_hal::{
+    devices::{
+        EthercatDevice, downcast_device,
+        wago_750_354::{WAGO_750_354_IDENTITY_A, Wago750_354},
+        wago_modules::{wago_750_671::Wago750_671, wago_750_672::{self, Wago750_672}},
+    },
+    io::{stepper_velocity_wago_750_671::StepperVelocityWago750671, stepper_velocity_wago_750_672::StepperVelocityWago750672},
+};
+use smol::{block_on, lock::RwLock};
+use std::{sync::Arc, time::Instant};
+
+use crate::{
+    MachineNewHardware, MachineNewParams, MachineNewTrait, get_ethercat_device,
+    validate_no_role_duplicates, validate_same_machine_identification_unique,
+};
+
+use anyhow::Error;
+
+
+
+
+impl MachineNewTrait for TestMachineStepper {
+    fn new<'maindevice>(params: &MachineNewParams) -> Result<Self, Error> {
+        // validate general stuff
+        let device_identification = params
+            .device_group
+            .iter()
+            .map(|device_identification| device_identification.clone())
+            .collect::<Vec<_>>();
+        validate_same_machine_identification_unique(&device_identification)?;
+        validate_no_role_duplicates(&device_identification)?;
+
+        let hardware = match &params.hardware {
+            MachineNewHardware::Ethercat(x) => x,
+            _ => {
+                return Err(anyhow::anyhow!(
+                    "[{}::MachineNewTrait/TestMachineStepper::new] MachineNewHardware is not Ethercat",
+                    module_path!()
+                ));
+            }
+        };
+        block_on(async {
+            let _wago_750_354 = get_ethercat_device::<Wago750_354>(
+                hardware,
+                params,
+                0,
+                [WAGO_750_354_IDENTITY_A].to_vec(),
+            )
+            .await?;
+
+            let modules = Wago750_354::initialize_modules(_wago_750_354.1).await?;
+
+            let mut coupler = _wago_750_354.0.write().await;
+
+            for module in modules {
+                coupler.set_module(module);
+            }
+
+            coupler.init_slot_modules(_wago_750_354.1);
+            let dev = coupler.slot_devices.first().unwrap().clone().unwrap();
+
+            // A little quick and dirty, but  this allows 750_671 and 672
+            let res = downcast_device::<Wago750_672>(dev).await;
+            if res.is_ok() {
+                let dev = coupler.slot_devices.first().unwrap().clone().unwrap();
+                let wago_750_672 =
+                    downcast_device::<Wago750_672>(dev).await?;
+                let stepper = super::Stepper::Wago750_672( StepperVelocityWago750672::new(wago_750_672) );
+                let (sender, receiver) = smol::channel::unbounded();
+                let mut my_test = Self {
+                    api_receiver: receiver,
+                    api_sender: sender,
+                    machine_identification_unique: params.get_machine_identification_unique(),
+                    namespace: TestMachineStepperNamespace {
+                        namespace: params.namespace.clone(),
+                    },
+                    last_state_emit: Instant::now(),
+                    main_sender: params.main_thread_channel.clone(),
+                    stepper,
+                };
+                my_test.emit_state();
+                drop(coupler);
+                return Ok(my_test);
+            }else{
+                let dev = coupler.slot_devices.first().unwrap().clone().unwrap();
+                let wago_750_671 =
+                    downcast_device::<Wago750_671>(dev).await?;
+                let stepper = super::Stepper::Wago750_671( StepperVelocityWago750671::new(wago_750_671) );
+                let (sender, receiver) = smol::channel::unbounded();
+                let mut my_test = Self {
+                    api_receiver: receiver,
+                    api_sender: sender,
+                    machine_identification_unique: params.get_machine_identification_unique(),
+                    namespace: TestMachineStepperNamespace {
+                        namespace: params.namespace.clone(),
+                    },
+                    last_state_emit: Instant::now(),
+                    main_sender: params.main_thread_channel.clone(),
+                    stepper,
+                };
+                my_test.emit_state();
+                drop(coupler);
+                return Ok(my_test);
+            }
+
+            
+            
+        })
+    }
+}
