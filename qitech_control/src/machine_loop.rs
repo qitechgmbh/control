dc5020eb02791602b36ed4ba89bc84904f14ed69
kraemr <93094049+kraemr@users.noreply.github.com>
2026-04-30 18:14:19 +0200

    fixed bug introduced by "new" mapping ...

--- qitech_control/src/machine_loop.rs
+++ qitech_control/src/machine_loop.rs
@@ -2,7 +2,7 @@ use bitvec::{order::Lsb0, slice::BitSlice};
 use machine_implementations::QiTechMachine;
 use qitech_lib::{
     ethercat_hal::{
-        Consumer, Producer, controller::{EtherCATAppHandle, EtherCATController}, devices::EthercatDevice
+        Consumer, MetaSubdevice, Producer, controller::{EtherCATAppHandle, EtherCATController}, devices::EthercatDevice
     },
     machines::MachineDataRegistry,
 };
@@ -10,39 +10,40 @@ use std::{cell::RefCell, rc::Rc, sync::Arc};
 
 pub fn write_ecat_inputs<C : Consumer,P: Producer>(
     ecat: &mut EtherCATAppHandle<C,P>,
-    ecat_controller: Arc<EtherCATController<C,P>>,
-    subdevices: Vec<Rc<RefCell<dyn EthercatDevice>>>,
+    subdevices: Vec<(MetaSubdevice, Rc<RefCell<dyn EthercatDevice>>)>,
 ) {
-    assert!(ecat_controller.subdevice_count == subdevices.len());
+    //assert!(subdevices.subdevice_count == subdevices.len());
     let inputs = ecat.get_inputs();
-    for i in 0..ecat_controller.subdevice_count {
-        let meta_dev = ecat_controller.subdevices[i];
+   // println!("######");
+    for i in 0..subdevices.len() {
+        let meta_dev = subdevices[i].0;
         let subdevice = subdevices.get(i).unwrap();
         let input_slice = &inputs[meta_dev.start_tx..meta_dev.end_tx];
         let input_bits_slice = BitSlice::<u8, Lsb0>::from_slice(input_slice);
+     //   println!("{:?}",input_bits_slice);
         {
-            let mut subdevice = subdevice.borrow_mut();
+            let mut subdevice = subdevice.1.borrow_mut();
             let _res = subdevice.input(input_bits_slice);
             let _res = subdevice.input_post_process();
         }
     }
+    //println!("######");
 //    println!("ecat_controller cycle_time us: {}",ecat_controller.cycle_time_us);
 }
 
 pub fn write_ecat_outputs<C : Consumer,P: Producer>(
     ecat: &mut EtherCATAppHandle<C,P>,
-    ecat_controller: Arc<EtherCATController<C,P>>,
-    subdevices: Vec<Rc<RefCell<dyn EthercatDevice>>>,
+    subdevices: Vec<(MetaSubdevice, Rc<RefCell<dyn EthercatDevice>>)>,
 ) {
-    assert!(ecat_controller.subdevice_count == subdevices.len());
-    let outputs = ecat.write_outputs();
-    for i in 0..ecat_controller.subdevice_count {
-        let meta_dev = ecat_controller.subdevices[i];
+    //assert!(ecat_controller.subdevice_count == subdevices.len());
+    let outputs = ecat.write_outputs();    
+    for i in 0..subdevices.len() {
+        let meta_dev = subdevices[i].0;
         let subdevice = subdevices.get(i).unwrap();
         let output_slice = &mut outputs[meta_dev.start_rx..meta_dev.end_rx];
         let output_bits = BitSlice::<u8, Lsb0>::from_slice_mut(output_slice);
         {
-            let mut subdevice = subdevice.borrow_mut();
+            let mut subdevice = subdevice.1.borrow_mut();
             let _res = subdevice.output(output_bits);
             let _res = subdevice.output_pre_process();
         }
@@ -52,11 +53,12 @@ pub fn write_ecat_outputs<C : Consumer,P: Producer>(
 
 pub fn run_machines(machines: &mut Vec<Box<dyn QiTechMachine>>, reg: &mut MachineDataRegistry) {
     let machine_count = machines.len();
+  //  println!("machine_count: {}",machines.len());
     for i in 0..machine_count {
         let machine = machines
             .get_mut(i)
             .expect("Machine should NEVER be NONE here (run_machines)!!");
-        reg.zero_entry(machine.get_identification());
+       // reg.zero_entry(machine.get_identification());
         machine.act(Some(reg));
     }
 
--- qitech_control/src/main.rs
+++ qitech_control/src/main.rs
@@ -1,13 +1,21 @@
 use apis::socketio::queue::start_socketio_queue;
 use app_state::SharedAppState;
 use machine_implementations::registry::MACHINE_REGISTRY;
-use machine_loop::{run_machines, write_ecat_inputs, write_ecat_outputs};
 use qitech_lib::ethercat_hal::controller::{TripleBufConsumer, TripleBufProducer};
 use qitech_lib::modbus::clients::example_client::ExampleClient;
 use qitech_lib::modbus::managers::ExampleDeviceManager;
 use qitech_lib::modbus::start_modbus_async_task;
 use qitech_lib::{ethercat_hal::devices::{device_from_subdevice_identity_rc}};
-use qitech_lib::ethercat_hal::{EtherCATControl, init_ethercat};
+
+use qitech_lib::ethercat_hal::EtherCATControl;
+
+#[cfg(not(feature = "mock"))]
+use qitech_lib::ethercat_hal::init_ethercat;
+#[cfg(not(feature = "mock"))]
+use machine_loop::{run_machines, write_ecat_inputs, write_ecat_outputs};
+
+
+
 use crate::app_state::get_async_runtime;
 use crate::app_state::MainState;
 
@@ -18,41 +26,32 @@ use std::{
 };
 
 pub mod apis;
+pub mod persist;
 mod app_state;
 mod machine_loop;
 mod mock;
-mod mock_main;
 
 fn setup_ethercat(
     state : Arc<SharedAppState>,
     main_state : &mut MainState ,
     eth_control : &EtherCATControl<TripleBufConsumer,TripleBufProducer>)
 {
-
     let _res = eth_control.channel.request_state_change(qitech_lib::ethercat_hal::EtherCATState::PreOp);
     std::thread::sleep(Duration::from_millis(5000));
-
+ 
     let mut idents = vec![];    
     println!("Initialized {} subdevices",eth_control.controller.subdevice_count);
-    
-    let mut devices_by_address = eth_control.controller
-        .get_subdevices()
-        .into_iter()
-        .map(|meta| {
-            let dev = device_from_subdevice_identity_rc(meta).unwrap();
-            (meta.device_address, (meta.clone(), dev))
-        })
-        .collect::<HashMap<_, _>>();
 
-    for (_, dev) in devices_by_address.values() {
-        main_state.subdevices.push(dev.clone());
+    for meta in eth_control.controller.get_subdevices() {
+        let dev = device_from_subdevice_identity_rc(&meta).unwrap();
+        main_state.subdevices.push((meta.clone(), dev.clone()));
     }
 
     match eth_control.channel.read_device_identifications() {
         Ok(mut eeprom_idents) => {
             main_state.generate_machine_hardware_from_ethercat(
                 &eeprom_idents,
-                &mut devices_by_address,
+                main_state.subdevices.clone(),
                 eth_control.channel.clone(),
             );
             idents.append(&mut eeprom_idents);
@@ -85,7 +84,7 @@ fn send_setup_done_events(state : Arc<SharedAppState>){
     });
 }
 
-fn setup_api(state : Arc<SharedAppState>){
+fn setup_api_and_websock(state : Arc<SharedAppState>){
     let rt = get_async_runtime();
     rt.spawn(apis::init_api(state.clone()));    
     rt.spawn(start_socketio_queue(state));
@@ -117,7 +116,8 @@ fn main_logic(){
     let state = Arc::new(SharedAppState::new());
     let mut main_state = MainState::new();
     let mut eth_control = init_ethercat("eth0");
-    setup_api(state.clone());    
+    
+    setup_api_and_websock(state.clone());    
     setup_ethercat(state.clone(),&mut main_state,&eth_control);
     setup_serial(&mut main_state);
     println!("Initialized {} hw",main_state.hardware.keys().len());
@@ -130,13 +130,11 @@ fn main_logic(){
     loop {
         write_ecat_inputs(
             &mut eth_control.app_handle,
-            eth_control.controller.clone(),
             main_state.subdevices.clone(),
         );
         run_machines(&mut main_state.machines, &mut main_state.machine_data_reg);
         write_ecat_outputs(
             &mut eth_control.app_handle,
-            eth_control.controller.clone(),
             main_state.subdevices.clone(),
         );
         std::thread::sleep(Duration::from_micros(100));
@@ -145,7 +143,7 @@ fn main_logic(){
 
 fn main() {
     #[cfg(feature = "mock")]
-    mock_logic();
+    mock::mock_logic();
 
     #[cfg(not(feature = "mock"))]
     main_logic();  
