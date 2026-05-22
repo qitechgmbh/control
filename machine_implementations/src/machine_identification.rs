0c004e310238bf15089a46859695336daa71b979
Hans Sebastian Pöhlmann <contact@hsp.dev>
2026-05-22 15:54:03 +0200

    registered machines

--- machine_implementations/src/machine_identification.rs
+++ machine_implementations/src/machine_identification.rs
@@ -107,6 +107,13 @@ impl MachineIdentification {
             x if x == WAGO_DO_TEST_MACHINE => "wago_do_test_machine".to_string(),
             x if x == WAGO_750_430_DI_MACHINE => "wago_750_430_di_machine".to_string(),
             x if x == DIGITAL_INPUT_TEST_MACHINE => "digital_input_test_machine".to_string(),
+            x if x == WAGO_8CH_IO_TEST_MACHINE => "wago_8ch_dio_test_machine".to_string(),
+            x if x == WAGO_750_460_MACHINE => "wago_750_460_machine".to_string(),
+            x if x == WAGO_750_501_TEST_MACHINE => "wago_750_501_test_machine".to_string(),
+            x if x == WAGO_750_531_MACHINE => "wago_750_531_machine".to_string(),
+            x if x == WAGO_750_553_MACHINE => "wago_750_553_machine".to_string(),
+            x if x == TEST_MACHINE_STEPPER => "test_machine_stepper".to_string(),
+            x if x == MOTOR_TEST_MACHINE => "motor_test_machine".to_string(),
             _ => unreachable!("Unknown machine id"),
         }
     }
@@ -211,7 +218,14 @@ use crate::MACHINE_WAGO_POWER_V1;
 use crate::MACHINE_WINDER_V1;
 use crate::TEST_MACHINE;
 use crate::VENDOR_QITECH;
+use crate::MOTOR_TEST_MACHINE;
+use crate::TEST_MACHINE_STEPPER;
 use crate::WAGO_750_430_DI_MACHINE;
+use crate::WAGO_750_460_MACHINE;
+use crate::WAGO_750_501_TEST_MACHINE;
+use crate::WAGO_750_531_MACHINE;
+use crate::WAGO_750_553_MACHINE;
+use crate::WAGO_8CH_IO_TEST_MACHINE;
 use crate::WAGO_AI_TEST_MACHINE;
 use crate::WAGO_DO_TEST_MACHINE;
 use anyhow::Error;
