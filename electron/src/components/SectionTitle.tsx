eceb4222a4b3ef387d9a6e58030b7e002a6380d5
Christian <80114847+TheBest6337@users.noreply.github.com>
2026-06-22 15:34:05 +0200

    Add reload button to update tab

--- electron/src/components/SectionTitle.tsx
+++ electron/src/components/SectionTitle.tsx
@@ -3,13 +3,17 @@ import React from "react";
 type Props = {
   title: string;
   children?: React.ReactNode;
+  right?: React.ReactNode;
 };
 
-export function SectionTitle({ children, title }: Props) {
+export function SectionTitle({ children, title, right }: Props) {
   return (
-    <div className="flex gap-4">
-      <h1 className="text-2xl">{title}</h1>
-      {children}
+    <div className="flex items-center justify-between gap-4">
+      <div className="flex items-center gap-4">
+        <h1 className="text-2xl">{title}</h1>
+        {children}
+      </div>
+      {right}
     </div>
   );
 }
