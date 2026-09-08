fa8bd3920ea2472a9a4efa0b425cb01a7db46301
Christian <80114847+TheBest6337@users.noreply.github.com>
2026-09-08 16:11:19 +0200

    fmt

--- qitech_control/src/api/legacy/v1/mod.rs
+++ qitech_control/src/api/legacy/v1/mod.rs
@@ -5,8 +5,8 @@ use qitech_framework_hub::ActorContext;
 use tokio::sync::mpsc;
 
 pub mod machine_mutate;
-mod response_util;
 pub mod modbus;
+mod response_util;
 pub mod write_machine_device_identification;
 
 pub fn router() -> Router<(ActorContext, mpsc::Sender<MachineInstanceIdentification>)> {
